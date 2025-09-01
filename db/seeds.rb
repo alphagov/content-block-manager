e8a902c2b47ed08af8063e568d9c9fcc5a8f8f41
Ed Davey <ed.davey@digital.cabinet-office.gov.uk>
2025-09-01 11:15:54 +0100

    Remove org assocation from user seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,8 @@
 return if Rails.env.test?
 
-gds_organisation_id = "af07d5a5-df63-4ddc-9383-6a666845ebe9"
 User.create!(
   name: "Test user",
   uid: "test-user-1",
   email: "test@gds.example.com",
   permissions: ["signin", "GDS Admin", "GDS Editor", "Managing Editor", "Sidekiq Admin"],
-  organisation_content_id: gds_organisation_id,
-  organisation_slug: "government-digital-service",
 )
