462845308bcf8ddbf8c0c73a57e676225db5b46d
pezholio <pezholio@gmail.com>
2025-09-02 15:28:15 +0100

    Alter assets prefix
    Should be named the same as the repo.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 # Path within public/ where assets are compiled to
-Rails.application.config.assets.prefix = "/assets/content_block_manager"
+Rails.application.config.assets.prefix = "/assets/content-block-manager"
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = "1.0"
