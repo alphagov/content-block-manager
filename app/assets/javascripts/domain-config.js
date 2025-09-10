94c9bc154567a29bd6022733a04ff4b23eb929fd
pezholio <pezholio@gmail.com>
2025-09-10 14:51:51 +0100

    Update domain-config
    Looking at these values, they seem to be the [same across apps](https://github.com/search?q=org%3Aalphagov+GTM-P93SHJ4Z&type=code)
    so we should be good to put these in here.

--- app/assets/javascripts/domain-config.js
+++ app/assets/javascripts/domain-config.js
@@ -6,8 +6,8 @@ window.GOVUK.vars.extraDomains = [
     name: 'production',
     domains: ['content-block-manager.publishing.service.gov.uk'],
     initialiseGA4: true,
-    id: 'GTM-xxx',
-    gaProperty: 'UA-xxx'
+    id: 'GTM-P93SHJ4Z',
+    gaProperty: 'UA-26179049-6'
   },
   {
     name: 'staging',
@@ -18,8 +18,8 @@ window.GOVUK.vars.extraDomains = [
     name: 'integration',
     domains: ['content-block-manager.integration.publishing.service.gov.uk'],
     initialiseGA4: true,
-    id: 'GTM-xxx',
-    auth: 'xxxx',
+    id: 'GTM-P93SHJ4Z',
+    auth: '8jHx-VNEguw67iX9TBC6_g',
     preview: 'env-50'
   }
 ]
