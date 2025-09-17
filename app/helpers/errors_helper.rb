20221590f664c9fd6715f9242e981f0fbf0e44bf
pezholio <pezholio@gmail.com>
2025-09-17 12:10:33 +0100

    Remove unused error helpers

--- app/helpers/errors_helper.rb
+++ app/helpers/errors_helper.rb
@@ -1,17 +1,4 @@
 module ErrorsHelper
-  def errors_for_input(errors, attribute)
-    return nil if errors.blank?
-
-    errors.filter_map { |error|
-      if error.attribute == attribute
-        error.full_message
-      end
-    }
-    .join(tag.br)
-    .html_safe
-    .presence
-  end
-
   def errors_for(errors, attribute)
     return nil if errors.blank?
 
@@ -23,16 +10,5 @@ module ErrorsHelper
       end
     }
     .presence
-  end
-
-  def errors_from_flash(flash)
-    return nil if flash.blank?
-
-    flash.map do |array|
-      {
-        href: "##{array.first}",
-        text: array.last,
-      }
-    end
   end
 end
