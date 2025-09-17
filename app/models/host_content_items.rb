9b016511575ec00c1c6f6e42e4aa23d9c981b028
pezholio <pezholio@gmail.com>
2025-09-17 12:04:00 +0100

    Remove unused `HostContentItems` class

--- app/models/host_content_items.rb
+++
@@ -1,9 +0,0 @@
-class HostContentItems < Data.define(:items, :total, :total_pages, :rollup)
-  extend Forwardable
-
-  ARRAY_METHODS = ([].methods - Object.methods)
-
-  def_delegators :items, *ARRAY_METHODS
-
-  class Rollup < Data.define(:views, :locations, :instances, :organisations); end
-end
