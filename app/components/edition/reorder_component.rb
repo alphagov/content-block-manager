8649bb22d39ef578230c259549bbfa04cbc6b20f
pezholio <pezholio@gmail.com>
2025-09-30 12:13:40 +0100

    Render up and down buttons to move the blocks
    This adds buttons to link to the edit controller action with a new
    `order` param that specifies the order of the blocks. In the next commit
    we’ll update the controller to use these params.

--- app/components/edition/reorder_component.rb
+++ app/components/edition/reorder_component.rb
@@ -7,4 +7,14 @@ class Edition::ReorderComponent < ViewComponent::Base
 private
 
   attr_reader :edition, :order
+
+  def move_path(position, item, direction)
+    updated_order = order.dup
+    new_position = direction == :up ? position - 1 : position + 1
+    updated_order.insert(
+      new_position,
+      updated_order.delete(item),
+    )
+    order_edit_edition_path(edition, order: order_dup)
+  end
 end
--- test/components/edition/reorder_component_test.rb
+++ test/components/edition/reorder_component_test.rb
@@ -2,6 +2,7 @@ require "test_helper"
 
 class Edition::ReorderComponentTest < ViewComponent::TestCase
   extend Minitest::Spec::DSL
+  include Rails.application.routes.url_helpers
 
   let(:edition) { build_stubbed(:edition) }
   let(:order) do
@@ -38,5 +39,47 @@ class Edition::ReorderComponentTest < ViewComponent::TestCase
         item.assert_selector ".govspeak", text: "telephones.telephone_1"
       end
     end
+  end
+
+  it "renders up and down buttons with the correct paths" do
+    render_inline component
+
+    assert_selector ".app-c-content-block-manager-reorder-component" do |wrapper|
+      wrapper.assert_selector ".app-c-content-block-manager-reorder-component__item:nth-child(2)" do |item|
+        item.assert_no_selector "a", text: "Up"
+        assert_button_exists(wrapper: item, label: "Down", order: %w[
+          email_addresses.email_address_2
+          email_addresses.email_address_1
+          telephones.telephone_1
+        ])
+      end
+
+      wrapper.assert_selector ".app-c-content-block-manager-reorder-component__item:nth-child(3)" do |item|
+        assert_button_exists(wrapper: item, label: "Up", order: %w[
+          email_addresses.email_address_2
+          email_addresses.email_address_1
+          telephones.telephone_1
+        ])
+        assert_button_exists(wrapper: item, label: "Down", order: %w[
+          email_addresses.email_address_1
+          telephones.telephone_1
+          email_addresses.email_address_2
+        ])
+      end
+
+      wrapper.assert_selector ".app-c-content-block-manager-reorder-component__item:nth-child(4)" do |item|
+        assert_button_exists(wrapper: item, label: "Up", order: %w[
+          email_addresses.email_address_1
+          telephones.telephone_1
+          email_addresses.email_address_2
+        ])
+        item.assert_no_selector "a", text: "Down"
+      end
+    end
+  end
+
+  def assert_button_exists(wrapper:, label:, order:)
+    href = order_edit_edition_path(edition, order:)
+    wrapper.assert_selector "a[href='#{href}']", text: label
   end
 end
