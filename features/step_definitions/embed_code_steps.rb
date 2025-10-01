df3998e0905521fc07660463a5df6d1415cb4c75
Ed Davey <ed.davey@digital.cabinet-office.gov.uk>
2025-10-01 17:42:28 +0100

    Describe new behaviour of embed code display

--- features/step_definitions/embed_code_steps.rb
+++ features/step_definitions/embed_code_steps.rb
@@ -17,6 +17,19 @@ Then("the embed code for the pension rate should be visible") do
   expect(page).to have_content(pension_rate_embed_code)
 end
 
+Then("I should not see the embed code displayed for the pension rate") do
+  expect(page).to have_no_content(pension_rate_embed_code)
+end
+
+Then("I should see the embed code flash up for an interval") do
+  Capybara.current_session.driver.with_playwright_page do |p_page|
+    block = p_page.get_by_testid("rates_listing")
+    expect(block).to playwright_matchers.have_text(pension_rate_embed_code)
+    # a short interval of time passes
+    expect(block).not_to playwright_matchers.have_text(pension_rate_embed_code)
+  end
+end
+
 def pension_rate_embed_code
   @pension_rate_embed_code ||=
     Document.find_by!(block_type: "pension")
