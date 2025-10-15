87182a70772c6cd91564e8cd58a21934ed2e829c
Ed Davey <ed.davey@digital.cabinet-office.gov.uk>
2025-10-15 17:17:02 +0100

    Refactor embed code feature
    We make these embed code step helpers work for both pension
    and contact test fixtures in advance of adding scenarios
    describing the "contact" default block's embed codes.

--- features/step_definitions/embed_code_steps.rb
+++ features/step_definitions/embed_code_steps.rb
@@ -1,32 +1,60 @@
 When("I click to copy the embed code for the pension rate") do
   within(".govuk-summary-list__row", text: "Amount") do
-    find("a", text: "Copy code").click
-    has_text?("Code copied")
+    click_copy_code_link
   end
 end
 
-Then("the embed code should be copied to my clipboard") do
+When("I click to copy the embed code for the contact's default block") do
+  within(".gem-c-summary-card", text: "Default block") do
+    click_copy_code_link
+  end
+end
+
+Then(/the ([^"]*) embed code should be copied to my clipboard/) do |code_type|
   Capybara.current_session.driver.with_playwright_page do |page|
     page.context.grant_permissions(%w[clipboard-read])
   end
   clip_text = page.evaluate_async_script("navigator.clipboard.readText().then(arguments[0])")
-  expect(clip_text).to eq(pension_rate_embed_code)
+  expect(clip_text).to eq(embed_code_for(code_type))
 end
 
-Then("the embed code for the pension rate should be visible") do
-  expect(page).to have_content(pension_rate_embed_code)
+Then(/the ([^"]*) embed code should be visible/) do |code_type|
+  expect(page).to have_content(embed_code_for(code_type))
 end
 
-Then("I should not see the embed code displayed for the pension rate") do
-  expect(page).to have_no_content(pension_rate_embed_code)
+Then(/I should not see the ([^"]*) embed code displayed/) do |code_type|
+  expect(page).to have_no_content(embed_code_for(code_type))
 end
 
-Then("I should see the embed code flash up for an interval") do
+Then(/I should see the ([^"]*) embed code flash up for an interval/) do |code_type|
   Capybara.current_session.driver.with_playwright_page do |p_page|
-    block = p_page.get_by_testid("rates_listing")
-    expect(block).to playwright_matchers.have_text(pension_rate_embed_code)
+    block = p_page.get_by_testid(test_block_id_for(code_type))
+    expect(block).to playwright_matchers.have_text(embed_code_for(code_type))
     # a short interval of time passes
-    expect(block).not_to playwright_matchers.have_text(pension_rate_embed_code)
+    expect(block).not_to playwright_matchers.have_text(embed_code_for(code_type))
+  end
+end
+
+def click_copy_code_link
+  find("a", text: "Copy code").click
+  has_text?("Code copied")
+end
+
+def test_block_id_for(code_type)
+  case code_type
+  when "pension rate"
+    "rates_listing"
+  when "contact default block"
+    "default_block"
+  end
+end
+
+def embed_code_for(code_type)
+  case code_type
+  when "pension rate"
+    pension_rate_embed_code
+  when "contact default block"
+    contact_default_block_embed_code
   end
 end
 
@@ -34,4 +62,10 @@ def pension_rate_embed_code
   @pension_rate_embed_code ||=
     Document.find_by!(block_type: "pension")
       .embed_code_for_field("rates/my-rate/amount")
+end
+
+def contact_default_block_embed_code
+  @contact_default_block_embed_code ||=
+    Document.find_by!(block_type: "contact")
+      .embed_code
 end
--- features/view_object.feature
+++ features/view_object.feature
@@ -83,16 +83,15 @@ Feature: View a content object
   @javascript
   Scenario: GDS Editor can copy embed code for a specific field
     When I visit the Content Block Manager home page
-    When I click to view the document with title "My pension"
-    Then I should not see the embed code displayed for the pension rate
+    And I click to view the document with title "My pension"
+    Then I should not see the pension rate embed code displayed
     
     When I click to copy the embed code for the pension rate
-    Then the embed code should be copied to my clipboard
-    And I should see the embed code flash up for an interval
+    Then the pension rate embed code should be copied to my clipboard
+    And I should see the pension rate embed code flash up for an interval
 
   Scenario: GDS Editor without javascript can see embed code
     When I visit the Content Block Manager home page
-    When I click to view the document with title "My pension"
-    Then the embed code for the pension rate should be visible
-
+    And I click to view the document with title "My pension"
+    Then the pension rate embed code should be visible
 
