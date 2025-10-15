c1b218979b6201fb4ddd7fd671bc3f22eb1f0a5c
pezholio <pezholio@gmail.com>
2025-10-15 16:27:38 +0100

    Use translated title in the summary list component
    I’ve also added some more comprehensive unit tests

--- test/components/document/show/summary_list_component_test.rb
+++ test/components/document/show/summary_list_component_test.rb
@@ -62,4 +62,152 @@ class Document::Show::SummaryListComponentTest < ViewComponent::TestCase
       assert_selector ".govuk-summary-list__value p", text: "instructions"
     end
   end
+
+  describe "#title_item" do
+    before do
+      I18n.expects(:t).with("activerecord.attributes.edition/document.title.default").returns("Default Title")
+    end
+
+    it "uses the block type specific translation" do
+      I18n.expects(:t).with(
+        "activerecord.attributes.edition/document.title.#{document.block_type}",
+        default: "Default Title",
+      ).returns("Custom Title Label")
+
+      render_inline(Document::Show::SummaryListComponent.new(document:))
+
+      assert_selector ".govuk-summary-list__key", text: "Custom Title Label"
+      assert_selector ".govuk-summary-list__value", text: document.title
+    end
+
+    it "falls back to default translation when block type translation is missing" do
+      I18n.expects(:t).with(
+        "activerecord.attributes.edition/document.title.#{document.block_type}",
+        default: "Default Title",
+      ).returns("Default Title")
+
+      render_inline(Document::Show::SummaryListComponent.new(document:))
+
+      assert_selector ".govuk-summary-list__key", text: "Default Title"
+      assert_selector ".govuk-summary-list__value", text: document.title
+    end
+  end
+
+  describe "#organisation_item" do
+    it "renders the lead organisation name" do
+      render_inline(Document::Show::SummaryListComponent.new(document:))
+
+      assert_selector ".govuk-summary-list__key", text: "Lead organisation"
+      assert_selector ".govuk-summary-list__value", text: "Department for Example"
+    end
+  end
+
+  describe "#details_items" do
+    it "renders all fields from the schema" do
+      render_inline(Document::Show::SummaryListComponent.new(document:))
+
+      assert_selector ".govuk-summary-list__key", text: "Foo"
+      assert_selector ".govuk-summary-list__value", text: "bar"
+      assert_selector ".govuk-summary-list__key", text: "Something"
+      assert_selector ".govuk-summary-list__value", text: "else"
+    end
+
+    it "humanizes field names" do
+      fields_with_underscores = [
+        stub("field", name: "field_with_underscores"),
+      ]
+      schema = stub(:schema, block_display_fields: [], fields: fields_with_underscores)
+      document.stubs(:schema).returns(schema)
+      edition.details = { "field_with_underscores" => "test value" }
+
+      render_inline(Document::Show::SummaryListComponent.new(document:))
+
+      assert_selector ".govuk-summary-list__key", text: "Field with underscores"
+      assert_selector ".govuk-summary-list__value", text: "test value"
+    end
+  end
+
+  describe "#status_item" do
+    describe "when edition is scheduled" do
+      before do
+        edition.state = "scheduled"
+        edition.scheduled_publication = Time.zone.now + 2.days
+      end
+
+      it "displays scheduled status with edit link" do
+        render_inline(Document::Show::SummaryListComponent.new(document:))
+
+        assert_selector ".govuk-summary-list__key", text: "Status"
+        assert_selector ".govuk-summary-list__value", text: /Scheduled for publication at/
+        assert_selector ".govuk-summary-list__actions a[href='#{document_schedule_edit_path(document)}']"
+      end
+
+      it "includes visually hidden text in edit link" do
+        render_inline(Document::Show::SummaryListComponent.new(document:))
+
+        assert_selector ".govuk-summary-list__actions", text: "Edit"
+        assert_selector ".govuk-visually-hidden", text: "schedule"
+      end
+    end
+
+    describe "when edition is published" do
+      before do
+        edition.state = "published"
+        edition.updated_at = 3.days.ago
+      end
+
+      it "displays published status with date and creator" do
+        render_inline(Document::Show::SummaryListComponent.new(document:))
+
+        assert_selector ".govuk-summary-list__key", text: "Status"
+        assert_selector ".govuk-summary-list__value", text: /Published on/
+        assert_selector ".govuk-summary-list__value", text: /by #{edition.creator.name}/
+      end
+
+      it "does not display edit link" do
+        render_inline(Document::Show::SummaryListComponent.new(document:))
+
+        assert_no_selector ".govuk-summary-list__actions a[href='#{document_schedule_edit_path(document)}']"
+      end
+    end
+  end
+
+  describe "rendering the full component" do
+    it "renders all expected summary list rows for published edition" do
+      render_inline(Document::Show::SummaryListComponent.new(document:))
+
+      # Title, 2 details fields (foo, something), organisation, instructions, status
+      assert_selector ".govuk-summary-list__row", count: 6
+    end
+
+    it "compacts nil items from the list" do
+      # This test ensures the .compact call in #items works correctly
+      # by verifying all rows render without errors even if some items might be nil
+      render_inline(Document::Show::SummaryListComponent.new(document:))
+
+      assert_not page.has_selector?(".govuk-summary-list__row", text: "nil")
+    end
+  end
+
+  describe "#edition" do
+    it "memoizes the edition" do
+      component = Document::Show::SummaryListComponent.new(document:)
+
+      # Access edition multiple times
+      document.expects(:latest_edition).once.returns(edition)
+
+      render_inline(component)
+    end
+  end
+
+  describe "#schema" do
+    it "memoizes the schema" do
+      component = Document::Show::SummaryListComponent.new(document:)
+
+      # Access schema multiple times through details_items
+      document.expects(:schema).once.returns(schema_without_block_display_fields)
+
+      render_inline(component)
+    end
+  end
 end
