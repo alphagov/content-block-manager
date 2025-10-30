66d4b970e4dca7b8f0a98e8b65a68fe5209d07d3
pezholio <pezholio@gmail.com>
2025-10-30 11:04:58 +0000

    Add seeding for organisations
    My local Publishing API database recently got blown away and I lost the
    Organisations I did have. I decided to fix this once and for all and add
    some code to create Organisations using the Content Store and Publishing
    API.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,34 @@ User.create!(
   email: "test@gds.example.com",
   permissions: ["signin", "GDS Admin", "GDS Editor", "Managing Editor", "Sidekiq Admin"],
 )
+
+if Organisation.all.count.zero?
+  print "Seeding organisations from live GOV.UK Content Store"
+  organisation_response = GdsApi::ContentStore.new("https://www.gov.uk/api")
+                                              .content_item("/government/organisations")
+
+  organisation_response["details"].values.flatten.each do |organisation|
+    Services.publishing_api.put_content(organisation["content_id"], {
+      "base_path" => organisation["href"],
+      "details" => {
+        "acronym" => organisation["acronym"],
+        "analytics_identifier" => organisation["analytics_identifier"],
+        "brand" => organisation["brand"],
+        "logo" => organisation["logo"],
+      },
+      "document_type" => "organisation",
+      "publishing_app" => "content-block-manager",
+      "rendering_app" => "frontend",
+      "schema_name" => "organisation",
+      "title" => organisation["title"],
+      "routes" => [
+        {
+          "path" => organisation["href"],
+          "type" => "exact",
+        },
+      ],
+    })
+    Services.publishing_api.publish(organisation["content_id"], "major")
+    print "."
+  end
+end
