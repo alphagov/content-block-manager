f50e2372fd05a794d672d6968b695a5efec8a215
Ed Davey <ed.davey@digital.cabinet-office.gov.uk>
2025-11-05 17:36:27 +0000

    Add DocumentStatusTagComponent
    The tag translates the Edition's `state` property into
    user-facing "status" using the locale system.
    
    Colours are set by the component, as this won't change, for
    example, if we use a Welsh locale file for Welsh "status"
    translations.
    
    We verify that for every `Edition#available_states`:
    
    - we handle the scenario where a state does not have an
     assigned colour
    
    - the state's colour is recognised by the Design System and
      results in the `.govuk-tag--{colour}` class being applied.
      [See docs for info on supported colours][]
      (grey green turquoise blue light-blue purple pink red orange yellow)
    
    [See docs for info on supported colours]:
    https://design-system.service.gov.uk/components/tag/all-colours/

---
+++ app/components/shared/document_status_tag_component.rb
@@ -0,0 +1,25 @@
+class Shared::DocumentStatusTagComponent < ViewComponent::Base
+  class UnexpectedStatusError < RuntimeError; end
+
+  COLOURS = {
+    draft: "yellow",
+    awaiting_2i: "turquoise",
+    scheduled: "light-blue",
+    published: "green",
+    superseded: "orange",
+  }.freeze
+
+  def initialize(document:)
+    @edition = document.most_recent_edition
+  end
+
+  def status
+    I18n.t("edition.states.label.#{@edition.state}")
+  end
+
+  def colour
+    COLOURS.fetch(@edition.state.to_sym) do
+      raise UnexpectedStatusError, "No colour mapped for state '#{@edition.state}'"
+    end
+  end
+end
---
+++ spec/components/shared/document_status_tag_component_spec.rb
@@ -0,0 +1,79 @@
+RSpec.describe Shared::DocumentStatusTagComponent, type: :component do
+  let(:edition) { instance_double(Edition, state: :draft) }
+  let(:document) { double(Document, most_recent_edition: edition) }
+
+  let(:component) { described_class.new(document: document) }
+
+  before do
+    allow(I18n).to receive(:t).and_return("Translated state")
+
+    render_inline(component)
+  end
+
+  it "finds document's most recent edition" do
+    expect(document).to have_received(:most_recent_edition)
+  end
+
+  it "sets a helpful 'title' attribute" do
+    expect(page).to have_css(".govuk-tag[title='Status: Translated state']")
+  end
+
+  it "sets  an aria label for screenreaders" do
+    expect(page).to have_css(".govuk-tag[aria-label='Status: Translated state']")
+  end
+
+  it "displays a translated version of the most recent edition's state" do
+    within ".govuk-tag" do
+      expect(page).to have_content("Translated state")
+    end
+  end
+
+  describe "#colour" do
+    expected_colours = {
+      draft: "yellow",
+      awaiting_2i: "turquoise",
+      scheduled: "light-blue",
+      published: "green",
+      superseded: "orange",
+    }
+
+    Edition.new.available_states.each do |state|
+      it "returns '#{expected_colours.fetch(state)}' for state '#{state}'" do
+        allow(edition).to receive(:state).and_return(state)
+
+        expect(component.colour).to eq(expected_colours.fetch(state))
+      end
+    end
+
+    context "if there's no colour mapped for the given state" do
+      before { allow(edition).to receive(:state).and_return(:unknown_state) }
+
+      it "raises an UnexpectedStatusError" do
+        expect { component.colour }.to raise_error(
+          Shared::DocumentStatusTagComponent::UnexpectedStatusError,
+          "No colour mapped for state 'unknown_state'",
+        )
+      end
+    end
+  end
+
+  describe "applies Design System colour styling successfully to govuk-tag" do
+    expected_colours = {
+      draft: "yellow",
+      awaiting_2i: "turquoise",
+      scheduled: "light-blue",
+      published: "green",
+      superseded: "orange",
+    }
+
+    Edition.new.available_states.each do |state|
+      it "sets the tag--{colour} '#{expected_colours.fetch(state)}' for state '#{state}'" do
+        allow(edition).to receive(:state).and_return(state)
+
+        render_inline(component)
+
+        expect(page).to have_css(".govuk-tag--#{expected_colours.fetch(state)}")
+      end
+    end
+  end
+end
