ab5fa830271394fd8d4b54e3dd8dd9f3eaf1369f
Graham MacGregor <graham.macgregor@digital.cabinet-office.gov.uk>
2025-11-06 11:33:50 +0000

    Refactor: Tidy up CanScheduleOrPublish module
    Removing the now-unused functions from this module and moving the
    is_scheduling? function to the Edition module. I believe this is a
    more correct place to put this function.

--- app/helpers/workflow_helper.rb
+++ app/helpers/workflow_helper.rb
@@ -1,11 +1,11 @@
 module WorkflowHelper
   def button_text_submit_for(edition)
-    return "Schedule" if is_scheduling?
+    return "Schedule" if edition.is_scheduling?
 
     edition.document.is_new_block? ? "Create" : "Publish"
   end
 
-  def save_action_value_for_edition
-    is_scheduling? ? "schedule" : "publish"
+  def save_action_value_for(edition)
+    edition.is_scheduling? ? "schedule" : "publish"
   end
 end
