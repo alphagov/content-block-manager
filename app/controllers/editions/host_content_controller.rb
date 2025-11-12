uk-gov-mirror/alphagov.content-block-manager

Add a controller method to handle preview form requests
The uses the form handler to fetch the redirect URL and then redirect to
the correct preview path with the service’s redirect path as the
`base_url` param, so we render this page in the preview frame.

app/controllers/editions/host_content_controller.rb
@@ -9,4 +9,22 @@ class Editions::HostContentController < BaseController
       locale: params[:locale],
     )
   end
+
+  def form_handler
+    form_handler = PreviewFormHandlerService.new(
+      url: params[:url].to_s,
+      form_body: params.permit(body: {}).fetch(:body).to_h,
+      method: params[:method].to_s,
+    )
+    edition = Edition.find(params[:id])
+
+    redirect_to host_content_preview_edition_path(
+      id: edition.id,
+      host_content_id: params[:host_content_id],
+      locale: params[:locale],
+      base_path: form_handler.response_location_path,
+    )
+  rescue PreviewFormHandlerService::UnexpectedResponseError, PreviewFormHandlerService::UnexpectedUrlError
+    head :bad_request
+  end
 end

spec/requests/editions/host_content_spec.rb
@@ -1,0 +1,70 @@
+RSpec.describe "Host content", type: :request do
+  include Rails.application.routes.url_helpers
+
+  setup do
+    logout
+    user = create(:user)
+    login_as(user)
+  end
+
+  describe "#form_handler" do
+    let(:host_content_id) { SecureRandom.uuid }
+    let(:locale) { "en" }
+    let(:params) do
+      {
+        id: 123,
+        url: "https://www.gov.uk/foo",
+        method: "post",
+        host_content_id:,
+        locale:,
+      }
+    end
+    let(:body) { { "foo" => "bar" } }
+    let(:edition) { build_stubbed(:edition) }
+
+    before do
+      allow(Edition).to receive(:find).with(params[:id].to_s).and_return(edition)
+    end
+
+    it "redirects to the response_location_path" do
+      form_handler_mock = instance_double(PreviewFormHandlerService, response_location_path: "/preview/123")
+
+      expect(PreviewFormHandlerService).to receive(:new)
+                                             .with(
+                                               url: params[:url],
+                                               form_body: body,
+                                               method: params[:method],
+                                             )
+                                             .and_return(form_handler_mock)
+
+      expected_redirect_path = host_content_preview_edition_path(
+        id: edition.id,
+        host_content_id: params[:host_content_id],
+        locale: params[:locale],
+        base_path: form_handler_mock.response_location_path,
+      )
+
+      post host_content_preview_form_handler_edition_path(params), params: { body: }
+
+      expect(response).to redirect_to(expected_redirect_path)
+    end
+
+    it "returns a bad request if the service raises an unexpected response error" do
+      expect(PreviewFormHandlerService).to receive(:new)
+                                       .and_raise(PreviewFormHandlerService::UnexpectedResponseError)
+
+      post host_content_preview_form_handler_edition_path(params), params: { body: }
+
+      expect(response.code).to eq("400")
+    end
+
+    it "returns a bad request if the service raises an unexpected url error" do
+      expect(PreviewFormHandlerService).to receive(:new)
+                                       .and_raise(PreviewFormHandlerService::UnexpectedResponseError)
+
+      post host_content_preview_form_handler_edition_path(params), params: { body: }
+
+      expect(response.code).to eq("400")
+    end
+  end
+end

alphagov/content-block-manager

app/controllers/editions/host_content_controller.rb
@@ -1,4 +1,6 @@
 class Editions::HostContentController < BaseController
+  skip_before_action :verify_authenticity_token, only: [:form_handler]
+
   def preview
     host_content_id = params[:host_content_id]
     @edition = Edition.find(params[:id])
@@ -8,5 +10,23 @@ class Editions::HostContentController < BaseController
       base_path: params[:base_path],
       locale: params[:locale],
     )
+  end
+
+  def form_handler
+    form_handler = FormRedirectExtractor.new(
+      url: params[:url].to_s,
+      form_body: params.permit(body: {}).fetch(:body).to_h,
+      method: params[:method].to_s,
+    )
+    edition = Edition.find(params[:id])
+
+    redirect_to host_content_preview_edition_path(
+      id: edition.id,
+      host_content_id: params[:host_content_id],
+      locale: params[:locale],
+      base_path: form_handler.response_location_path,
+    )
+  rescue FormRedirectExtractor::UnexpectedResponseError, FormRedirectExtractor::UnexpectedUrlError
+    head :bad_request
   end
 end

spec/requests/editions/host_content_spec.rb
@@ -1,0 +1,70 @@
+RSpec.describe "Host content", type: :request do
+  include Rails.application.routes.url_helpers
+
+  setup do
+    logout
+    user = create(:user)
+    login_as(user)
+  end
+
+  describe "#form_handler" do
+    let(:host_content_id) { SecureRandom.uuid }
+    let(:locale) { "en" }
+    let(:params) do
+      {
+        id: 123,
+        url: "https://www.gov.uk/foo",
+        method: "post",
+        host_content_id:,
+        locale:,
+      }
+    end
+    let(:body) { { "foo" => "bar" } }
+    let(:edition) { build_stubbed(:edition) }
+
+    before do
+      allow(Edition).to receive(:find).with(params[:id].to_s).and_return(edition)
+    end
+
+    it "redirects to the response_location_path" do
+      form_handler_mock = instance_double(FormRedirectExtractor, response_location_path: "/preview/123")
+
+      expect(FormRedirectExtractor).to receive(:new)
+                                             .with(
+                                               url: params[:url],
+                                               form_body: body,
+                                               method: params[:method],
+                                             )
+                                             .and_return(form_handler_mock)
+
+      expected_redirect_path = host_content_preview_edition_path(
+        id: edition.id,
+        host_content_id: params[:host_content_id],
+        locale: params[:locale],
+        base_path: form_handler_mock.response_location_path,
+      )
+
+      post host_content_preview_form_handler_edition_path(params), params: { body: }
+
+      expect(response).to redirect_to(expected_redirect_path)
+    end
+
+    it "returns a bad request if the service raises an unexpected response error" do
+      expect(FormRedirectExtractor).to receive(:new)
+                                       .and_raise(FormRedirectExtractor::UnexpectedResponseError)
+
+      post host_content_preview_form_handler_edition_path(params), params: { body: }
+
+      expect(response.code).to eq("400")
+    end
+
+    it "returns a bad request if the service raises an unexpected url error" do
+      expect(FormRedirectExtractor).to receive(:new)
+                                       .and_raise(FormRedirectExtractor::UnexpectedResponseError)
+
+      post host_content_preview_form_handler_edition_path(params), params: { body: }
+
+      expect(response.code).to eq("400")
+    end
+  end
+end