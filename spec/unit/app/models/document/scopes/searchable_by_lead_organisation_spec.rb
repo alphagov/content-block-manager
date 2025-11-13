9b36a2367e8970d8abc1cdbc1ca40e350dff02c5
Ed Davey <ed.davey@digital.cabinet-office.gov.uk>
2025-11-13 13:33:12 +0000

    Convert SearchByLeadOrganisation to RSpec

--- spec/unit/app/models/document/scopes/searchable_by_lead_organisation_spec.rb
+++ spec/unit/app/models/document/scopes/searchable_by_lead_organisation_spec.rb
@@ -1,20 +1,20 @@
-require "test_helper"
-
-class SearchableByLeadOrganisationTest < ActiveSupport::TestCase
-  extend Minitest::Spec::DSL
-
+RSpec.describe Document::Scopes::SearchableByLeadOrganisation do
   describe ".with_lead_organisation" do
     it "finds documents with lead organisation on latest edition" do
+      # NB "latest edition" requires the trait :latest to set Document#latest_edition_id
       matching_organisation = build(:organisation)
       document_with_org = create(:document, :pension)
       _edition_with_org = create(:edition,
                                  :pension,
+                                 :latest,
+                                 state: "published",
                                  document: document_with_org,
                                  lead_organisation_id: matching_organisation.id)
       document_without_org = create(:document, :pension)
       _edition_without_org = create(:edition, :pension, document: document_without_org)
       _document_without_latest_edition = create(:document, :pension)
-      assert_equal [document_with_org], Document.with_lead_organisation(matching_organisation.id)
+
+      expect(Document.with_lead_organisation(matching_organisation.id)).to eq([document_with_org])
     end
   end
 end
