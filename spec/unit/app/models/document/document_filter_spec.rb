4a914954bc0258c4bf1256cb91dd5b6cfdf66452
pezholio <pezholio@gmail.com>
2025-11-18 13:48:05 +0000

    Refactor document filter spec to use spies
    A lot of these tests set the expecations before the method under test is
    run. Now we’re using Rspec we can use [`spies`](https://rspec.info/features/3-13/rspec-mocks/basics/spies/)
    to have a more conventional structure to our tests

--- spec/unit/app/models/document/document_filter_spec.rb
+++ spec/unit/app/models/document/document_filter_spec.rb
@@ -6,139 +6,147 @@ RSpec.describe Document::DocumentFilter do
     allow(user).to receive(:is_e2e_user?).and_return(is_e2e_user)
     allow(Current).to receive(:user).and_return(user)
   end
+
   describe "paginated_documents" do
-    let(:document_scope_mock) { double }
+    let(:document_scope_spy) { spy }
 
     before do
       allow(Document).to receive(:where)
               .with(block_type: Schema.valid_schemas)
-              .and_return(document_scope_mock)
-      allow(document_scope_mock).to receive(:live).and_return(document_scope_mock)
-      allow(document_scope_mock).to receive(:joins).with(:latest_edition).and_return(document_scope_mock)
-      allow(document_scope_mock).to receive(:order).with("editions.updated_at DESC").and_return(document_scope_mock)
-      allow(document_scope_mock).to receive(:per).with(Document::DocumentFilter::DEFAULT_PAGE_SIZE).and_return([])
+              .and_return(document_scope_spy)
+
+      allow(document_scope_spy).to receive(:live).and_return(document_scope_spy)
+      allow(document_scope_spy).to receive(:where).and_return(document_scope_spy)
+      allow(document_scope_spy).to receive(:with_lead_organisation).and_return(document_scope_spy)
+      allow(document_scope_spy).to receive(:last_updated_after).and_return(document_scope_spy)
+      allow(document_scope_spy).to receive(:last_updated_before).and_return(document_scope_spy)
+      allow(document_scope_spy).to receive(:joins).with(:latest_edition).and_return(document_scope_spy)
+      allow(document_scope_spy).to receive(:page).and_return(document_scope_spy)
+      allow(document_scope_spy).to receive(:order).with("editions.updated_at DESC").and_return(document_scope_spy)
+      allow(document_scope_spy).to receive(:per).with(Document::DocumentFilter::DEFAULT_PAGE_SIZE).and_return([])
     end
 
     describe "when a user is not an e2e user" do
-      before do
-        allow(document_scope_mock)
-          .to receive(:where)
-          .with(testing_artefact: false)
-          .and_return(document_scope_mock)
+      it "only returns non-testing artefacts" do
+        Document::DocumentFilter.new({}).paginated_documents
+
+        expect(document_scope_spy).to have_received(:where).with(testing_artefact: false)
       end
 
       describe "when no filters are given" do
         it "returns live documents" do
-          expect(document_scope_mock).to receive(:page).with(1).and_return(document_scope_mock)
-
-          expect(document_scope_mock).to receive(:with_keyword).never
-          expect(document_scope_mock).to receive(:where).with(id: anything).never
-          expect(document_scope_mock).to receive(:with_lead_organisation).never
+          allow(Document).to receive(:with_keyword)
 
           Document::DocumentFilter.new({}).paginated_documents
+
+          expect(Document).to_not have_received(:with_keyword)
+          expect(document_scope_spy).to_not have_received(:where).with(id: anything)
+          expect(document_scope_spy).to_not have_received(:with_lead_organisation)
         end
       end
 
       describe "when a keyword filter is given" do
+        let(:keyword_spy) { spy }
+
+        before do
+          allow(Document).to receive(:with_keyword).and_return(keyword_spy)
+        end
+
         it "returns live documents with keyword" do
           ids = [1, 2, 3]
-          keyword_stub = double
-          allow(Document).to receive(:with_keyword)
-                  .with("ministry of example")
-                  .and_return(keyword_stub)
-          allow(keyword_stub).to receive(:pluck).with(:id).and_return(ids)
+          allow(keyword_spy).to receive(:pluck).with(:id).and_return(ids)
 
-          expect(document_scope_mock).to receive(:page).with(1).and_return(document_scope_mock)
-
-          expect(document_scope_mock).to receive(:where).with(id: ids).and_return(document_scope_mock)
           Document::DocumentFilter.new({ keyword: "ministry of example" }).paginated_documents
+
+          expect(Document).to have_received(:with_keyword).with("ministry of example")
+          expect(document_scope_spy).to have_received(:where).with(id: ids)
+          expect(document_scope_spy).to have_received(:page).with(1)
         end
       end
 
       describe "when a block type is given" do
         it "returns live documents of the type given" do
-          allow(document_scope_mock).to receive(:page).with(1).and_return(document_scope_mock)
-
-          expect(document_scope_mock).to receive(:where).with(block_type: %w[email_address]).and_return(document_scope_mock)
           Document::DocumentFilter.new({ block_type: %w[email_address] }).paginated_documents
+
+          expect(document_scope_spy).to have_received(:where).with(block_type: %w[email_address])
         end
       end
 
       describe "when a lead organisation id is given" do
         it "returns live documents with lead org given" do
-          allow(document_scope_mock).to receive(:page).with(1).and_return(document_scope_mock)
-
-          expect(document_scope_mock).to receive(:with_lead_organisation).with("123").and_return(document_scope_mock)
           Document::DocumentFilter.new({ lead_organisation: "123" }).paginated_documents
+
+          expect(document_scope_spy).to have_received(:with_lead_organisation).with("123")
         end
       end
 
       describe "when block types, keyword and organisation is given" do
+        let(:keyword_spy) { spy }
+        let(:keyword) { "ministry of example" }
+        let(:ids) { [1, 2, 3] }
+
+        before do
+          allow(Document).to receive(:with_keyword).and_return(keyword_spy)
+          allow(keyword_spy).to receive(:pluck).with(:id).and_return(ids)
+        end
+
         it "returns live documents with the filters given" do
-          allow(document_scope_mock).to receive(:page).with(1).and_return(document_scope_mock)
-
-          ids = [1, 2, 3]
-          keyword_stub = double
-          allow(Document).to receive(:with_keyword)
-                  .with("ministry of example")
-                  .and_return(keyword_stub)
-          allow(keyword_stub).to receive(:pluck).with(:id).and_return(ids)
-
-          expect(document_scope_mock).to receive(:where).with(id: ids).and_return(document_scope_mock)
-          expect(document_scope_mock).to receive(:where).with(block_type: %w[email_address]).and_return(document_scope_mock)
-          expect(document_scope_mock).to receive(:with_lead_organisation).with("123").and_return(document_scope_mock)
           Document::DocumentFilter.new(
             { block_type: %w[email_address], keyword: "ministry of example", lead_organisation: "123" },
           ).paginated_documents
+
+          expect(document_scope_spy).to have_received(:where).with(id: ids)
+          expect(document_scope_spy).to have_received(:where).with(block_type: %w[email_address])
+          expect(document_scope_spy).to have_received(:with_lead_organisation).with("123")
         end
       end
 
       describe "when a page is given" do
         it "passes the page to the query" do
-          expect(document_scope_mock).to receive(:page).with(2).and_return(document_scope_mock)
           Document::DocumentFilter.new({ page: 2 }).paginated_documents
+
+          expect(document_scope_spy).to have_received(:page).with(2)
         end
       end
 
       describe "last updated dates" do
         describe "when dates are missing" do
           it "does not filter by date if one or more date element is missing" do
-            expect(document_scope_mock).to receive(:page).with(1).and_return(document_scope_mock)
-
             Document::DocumentFilter.new(
               {
                 last_updated_from: { "3i" => "", "2i" => "2", "1i" => "2025" },
                 last_updated_to: { "3i" => "", "2i" => "", "1i" => ""  },
               },
             ).paginated_documents
+
+            expect(document_scope_spy).to_not have_received(:last_updated_before)
+            expect(document_scope_spy).to_not have_received(:last_updated_after)
           end
         end
 
         describe "when dates are valid" do
           it "filters using last updated from date" do
-            allow(document_scope_mock).to receive(:page).with(1).and_return(document_scope_mock)
-
             expected_date_time = Time.zone.local(2025, 2, 1)
 
-            expect(document_scope_mock).to receive(:last_updated_after).with(expected_date_time).and_return(document_scope_mock)
             Document::DocumentFilter.new(
               {
                 last_updated_from: { "3i" => "1", "2i" => "2", "1i" => "2025" },
               },
             ).paginated_documents
+
+            expect(document_scope_spy).to have_received(:last_updated_after).with(expected_date_time)
           end
 
           it "filters using last updated to date" do
-            allow(document_scope_mock).to receive(:page).with(1).and_return(document_scope_mock)
-
             expected_date_time = Time.zone.local(2026, 4, 3).end_of_day
 
-            expect(document_scope_mock).to receive(:last_updated_before).with(expected_date_time).and_return(document_scope_mock)
             Document::DocumentFilter.new(
               {
                 last_updated_to: { "3i" => "3", "2i" => "4", "1i" => "2026" },
               },
             ).paginated_documents
+
+            expect(document_scope_spy).to have_received(:last_updated_before).with(expected_date_time)
           end
         end
       end
@@ -148,14 +156,9 @@ RSpec.describe Document::DocumentFilter do
       let(:is_e2e_user) { true }
 
       it "does not filter by testing_artefact" do
-        allow(document_scope_mock).to receive(:page).with(1).and_return(document_scope_mock)
-
-        expect(document_scope_mock)
-          .to receive(:where)
-          .with(testing_artefact: false)
-          .never
-
         Document::DocumentFilter.new({}).paginated_documents
+
+        expect(document_scope_spy).to_not have_received(:where).with(testing_artefact: false)
       end
     end
 
