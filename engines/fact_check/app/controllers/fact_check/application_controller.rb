3866eca0166832247c871413af6cddb2d286b097
pezholio <pezholio@gmail.com>
2025-11-19 09:29:12 +0000

    Lock down the Fact Check application using GDS-SSO
    This will check if the user is logged in and has permissions to access
    Content Block Manager. In the future, we’ll add functionality to bypass
    this check via a token.

--- engines/fact_check/app/controllers/fact_check/application_controller.rb
+++ engines/fact_check/app/controllers/fact_check/application_controller.rb
@@ -1,5 +1,9 @@
 module FactCheck
   class ApplicationController < ActionController::Base
+    include GDS::SSO::ControllerMethods
+
+    prepend_before_action :authenticate_user!
+
     def product_name
       "Content Block Manager"
     end
