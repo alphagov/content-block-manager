06a98f6b28ede327dce9cf5d891b165f2875f73c
Graham MacGregor <graham.macgregor@digital.cabinet-office.gov.uk>
2025-11-19 11:56:03 +0000

    spec/unit/app/forms/edition_form_spec.rb

--- spec/unit/app/forms/edition_form_spec.rb
+++ spec/unit/app/forms/edition_form_spec.rb
@@ -1,8 +1,4 @@
-require "test_helper"
-
-class EditionFormTest < ActiveSupport::TestCase
-  extend Minitest::Spec::DSL
-
+RSpec.describe EditionForm do
   include Rails.application.routes.url_helpers
 
   let(:schema) { build(:schema, :pension, body: { "properties" => { "foo" => "", "bar" => "" } }) }
@@ -27,22 +23,22 @@ class EditionFormTest < ActiveSupport::TestCase
     it "initializes with the correct attributes from the schema" do
       expected_attributes = { "foo" => nil, "bar" => nil }
 
-      assert_equal edition, result.edition
-      assert_equal schema, result.schema
-      assert_equal expected_attributes, result.attributes
+      expect(result.edition).to eq(edition)
+      expect(result.schema).to eq(schema)
+      expect(result.attributes).to eq(expected_attributes)
     end
 
     it "sets the correct title" do
-      assert_equal I18n.t("edition.update.title", block_type: "pension"), result.title
+      expect(result.title).to eq(I18n.t("edition.update.title", block_type: "pension"))
     end
 
     it "sets the correct urls" do
-      assert_equal document_path(id: edition.document.id), result.back_path
-      assert_equal document_editions_path(document_id: edition.document.id), result.url
+      expect(result.back_path).to eq(document_path({ id: edition.document.id }))
+      expect(result.url).to eq(document_editions_path({ document_id: edition.document.id }))
     end
 
     it "sets the correct form method" do
-      assert_equal :post, result.form_method
+      expect(result.form_method).to eq(:post)
     end
 
     describe "when the errors include a sluggable_string error" do
@@ -53,8 +49,8 @@ class EditionFormTest < ActiveSupport::TestCase
       end
 
       it "removes the error from the object" do
-        assert_equal 1, result.edition.errors.count
-        assert_not_includes result.edition.errors.map(&:attribute), "document.sluggable_string".to_sym
+        expect(result.edition.errors.count).to eq(1)
+        expect(result.edition.errors.map(&:attribute)).not_to include("document.sluggable_string".to_sym)
       end
     end
   end
@@ -72,22 +68,22 @@ class EditionFormTest < ActiveSupport::TestCase
     it "initializes with the correct attributes from the schema" do
       expected_attributes = { "foo" => nil, "bar" => nil }
 
-      assert_equal edition, result.edition
-      assert_equal schema, result.schema
-      assert_equal expected_attributes, result.attributes
+      expect(result.edition).to eq(edition)
+      expect(result.schema).to eq(schema)
+      expect(result.attributes).to eq(expected_attributes)
     end
 
     it "sets the correct title" do
-      assert_equal I18n.t("edition.create.title", block_type: "pension"), result.title
+      expect(result.title).to eq(I18n.t("edition.create.title", block_type: "pension"))
     end
 
     it "sets the correct urls" do
-      assert_equal new_document_path, result.back_path
-      assert_equal editions_path, result.url
+      expect(result.back_path).to eq(new_document_path)
+      expect(result.url).to eq(editions_path)
     end
 
     it "sets the correct form method" do
-      assert_equal :post, result.form_method
+      expect(result.form_method).to eq(:post)
     end
 
     describe "when the errors include a sluggable_string error" do
@@ -98,8 +94,8 @@ class EditionFormTest < ActiveSupport::TestCase
       end
 
       it "removes the error from the object" do
-        assert_equal 1, result.edition.errors.count
-        assert_not_includes result.edition.errors.map(&:attribute), "document.sluggable_string".to_sym
+        expect(result.edition.errors.count).to eq(1)
+        expect(result.edition.errors.map(&:attribute)).not_to include("document.sluggable_string".to_sym)
       end
     end
   end
@@ -118,22 +114,22 @@ class EditionFormTest < ActiveSupport::TestCase
     it "initializes with the correct attributes from the schema" do
       expected_attributes = { "foo" => nil, "bar" => nil }
 
-      assert_equal edition, result.edition
-      assert_equal schema, result.schema
-      assert_equal expected_attributes, result.attributes
+      expect(result.edition).to eq(edition)
+      expect(result.schema).to eq(schema)
+      expect(result.attributes).to eq(expected_attributes)
     end
 
     it "sets the correct title" do
-      assert_equal I18n.t("edition.update.title", block_type: "pension"), result.title
+      expect(result.title).to eq(I18n.t("edition.update.title", block_type: "pension"))
     end
 
     it "sets the correct urls" do
-      assert_equal document_path(edition.document), result.back_path
-      assert_equal workflow_path(edition, step: "edit_draft"), result.url
+      expect(result.back_path).to eq(document_path(edition.document))
+      expect(result.url).to eq(workflow_path(edition, { step: "edit_draft" }))
     end
 
     it "sets the correct form method" do
-      assert_equal :put, result.form_method
+      expect(result.form_method).to eq(:put)
     end
 
     describe "when the errors include a sluggable_string error" do
@@ -144,25 +140,25 @@ class EditionFormTest < ActiveSupport::TestCase
       end
 
       it "removes the error from the object" do
-        assert_equal 1, result.edition.errors.count
-        assert_not_includes result.edition.errors.map(&:attribute), "document.sluggable_string".to_sym
+        expect(result.edition.errors.count).to eq(1)
+        expect(result.edition.errors.map(&:attribute)).not_to include("document.sluggable_string".to_sym)
       end
     end
 
     describe "when editing a new block" do
       before do
-        document.stubs(:is_new_block?).returns(true)
+        allow(document).to receive(:is_new_block?).and_return(true)
       end
 
       describe "#title" do
         it "returns a title for the create action" do
-          assert_equal I18n.t("edition.create.title", block_type: "pension"), result.title
+          expect(result.title).to eq(I18n.t("edition.create.title", block_type: "pension"))
         end
       end
 
       describe "#back_path" do
         it "returns the documents index path" do
-          assert_equal new_document_path, result.back_path
+          expect(result.back_path).to eq(new_document_path)
         end
       end
     end
