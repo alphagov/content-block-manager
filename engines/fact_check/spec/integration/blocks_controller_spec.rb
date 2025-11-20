3d0a9ecd87bdfaa5acbbbd785e1771a6141ceca5
pezholio <pezholio@gmail.com>
2025-11-20 15:26:07 +0000

    Add an endpoint to view a content block
    This adds a simple controller method and view to show the details
    of a content block.

---
+++ engines/fact_check/spec/integration/blocks_controller_spec.rb
@@ -0,0 +1,15 @@
+RSpec.describe FactCheck::BlocksController, type: :feature do
+  include FactCheck::Engine.routes.url_helpers
+
+  describe "#show" do
+    it "returns information about a block" do
+      block = build(:content_block)
+      expect(ContentBlock).to receive(:from_content_id_alias).with("some-content-block").and_return(block)
+
+      visit block_path("some-content-block")
+
+      expect(page).to have_css ".govuk-heading-xl", text: block.title
+      expect(page).to have_css ".govuk-caption-xl", text: block.block_type
+    end
+  end
+end
