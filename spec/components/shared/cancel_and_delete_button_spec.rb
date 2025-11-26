fbb0f4755193d145054f8e5a58f8b2f4fc3ca83b
pezholio <pezholio@gmail.com>
2025-11-26 11:12:47 +0000

    Don’t override the form’s name and value
    This is causing some issues with with CSRF token in the latest
    version of Rails. Removing this, and specifying the `delete` method
    directly in the call to `form_with` fixes this.

--- spec/components/shared/cancel_and_delete_button_spec.rb
+++ spec/components/shared/cancel_and_delete_button_spec.rb
@@ -9,7 +9,15 @@ RSpec.describe Shared::CancelAndDeleteButtonComponent, type: :component do
     render_inline(cancel_and_delete_component)
 
     expect(page).to have_css "form[action='#{url}']" do |form|
-      expect(form).to have_css "button[value='delete']", text: "Cancel"
+      expect(form).to have_css "button[type='submit']", text: "Cancel"
     end
+  end
+
+  it "calls form_with with the correct method" do
+    allow(cancel_and_delete_component).to receive(:form_with)
+
+    render_inline(cancel_and_delete_component)
+
+    expect(cancel_and_delete_component).to have_received(:form_with).with(url: url, method: :delete)
   end
 end
