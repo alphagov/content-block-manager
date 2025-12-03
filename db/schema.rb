b7d1d10363fd2511ba731c2eff5ff8de03c95762
Ed Davey <ed.davey@digital.cabinet-office.gov.uk>
2025-12-03 13:42:35 +0000

    Migrate existing versions in 'awaiting_2i state'
    Similar to [a2b5cf82eb0d27dd10b994c3b449f7aa7d331c0b][], the
    `versions` table also has a `state` field which gets set
    [on each state transition][].
    
    For the benefit of existing editions/versions in the
    integration and local development environments this data
    migration updates any versions to have the new
    "ready_for_review" state.
    
    [a2b5cf82eb0d27dd10b994c3b449f7aa7d331c0b]:
    https://github.com/alphagov/content-block-manager/blob/a2b5cf82eb0d27dd10b994c3b449f7aa7d331c0b/db/migrate/20251202172626_renname_awaiting2i_states_to_awaiting_review.rb
    
    [on each state transition]:
    https://github.com/alphagov/content-block-manager/blob/c898be18ca81f89a3e1d6aacfc32c609714828c9/app/models/edition/has_audit_trail.rb#L30-L41

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_12_03_131509) do
+ActiveRecord::Schema[8.0].define(version: 2025_12_03_134000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
