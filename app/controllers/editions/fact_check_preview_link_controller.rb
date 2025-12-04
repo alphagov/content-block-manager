efa5bbecf609bb20580d11ab796f9360f4afe9f1
pezholio <pezholio@gmail.com>
2025-12-04 15:07:03 +0000

    Use Turbo to render changes to the preview link
    This updates the preview link controller action to use Turbo Frames to
    replace the content of the frame with the new preview link if the
    request comes via Turbo. If it’s a standard request, we keep the
    existing behaviour.

--- app/controllers/editions/fact_check_preview_link_controller.rb
+++ app/controllers/editions/fact_check_preview_link_controller.rb
@@ -1,9 +1,12 @@
 class Editions::FactCheckPreviewLinkController < BaseController
   def update
-    edition = Edition.find(params[:id])
-    edition.set_auth_bypass_id
-    edition.save!
+    @edition = Edition.find(params[:id])
+    @edition.set_auth_bypass_id
+    @edition.save!
 
-    redirect_to document_path(edition.document), notice: "New preview link generated"
+    respond_to do |format|
+      format.html { redirect_to document_path(@edition.document), notice: "New preview link generated" }
+      format.turbo_stream { render :show }
+    end
   end
 end
