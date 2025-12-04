3b1fe219cc303fcb93d1197fac87b69c41e2d635
pezholio <pezholio@gmail.com>
2025-12-04 14:58:40 +0000

    Allow to fact check component to be rendered as open
    This will help us use Turbo to render the updated link in the next
    commit.

--- app/components/edition/show/share_fact_check_link_component.rb
+++ app/components/edition/show/share_fact_check_link_component.rb
@@ -1,9 +1,10 @@
 class Edition::Show::ShareFactCheckLinkComponent < ViewComponent::Base
-  def initialize(edition:)
+  def initialize(edition:, open: false)
     @edition = edition
+    @open = open
   end
 
 private
 
-  attr_reader :edition
+  attr_reader :edition, :open
 end
