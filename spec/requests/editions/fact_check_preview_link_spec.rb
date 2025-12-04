ecfddd02da6c7f2fcab3eea694f5e09b88ca556c
pezholio <pezholio@gmail.com>
2025-12-04 15:07:03 +0000

    Use Turbo to render changes to the preview link
    This updates the preview link controller action to use Turbo Frames to
    replace the content of the frame with the new preview link if the
    request comes via Turbo. If it’s a standard request, we keep the
    existing behaviour.

--- spec/requests/editions/fact_check_preview_link_spec.rb
+++ spec/requests/editions/fact_check_preview_link_spec.rb
@@ -13,12 +13,11 @@ RSpec.describe Editions::FactCheckPreviewLinkController, type: :request do
 
     before do
       allow(Edition).to receive(:find).with(edition.id.to_s).and_return(edition)
+      allow(edition).to receive(:set_auth_bypass_id)
+      allow(edition).to receive(:save!)
     end
 
     it "updates the edition and redirects to the document path" do
-      allow(edition).to receive(:set_auth_bypass_id)
-      allow(edition).to receive(:save!)
-
       put update_fact_check_preview_link_edition_path(edition)
 
       expect(response).to redirect_to(document_path(document))
@@ -26,6 +25,17 @@ RSpec.describe Editions::FactCheckPreviewLinkController, type: :request do
       expect(flash[:notice]).to eq("New preview link generated")
       expect(edition).to have_received(:set_auth_bypass_id)
       expect(edition).to have_received(:save!)
+    end
+
+    context "when the request is made as a turbo stream" do
+      it "renders a template" do
+        put update_fact_check_preview_link_edition_path(edition, format: :turbo_stream)
+
+        expect(response).to render_template("editions/fact_check_preview_link/show")
+
+        expect(edition).to have_received(:set_auth_bypass_id)
+        expect(edition).to have_received(:save!)
+      end
     end
   end
 end
