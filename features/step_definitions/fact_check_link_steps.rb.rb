f2f4604de9541fdf99fa6737ae8ce1679ab4b8e4
pezholio <pezholio@gmail.com>
2025-12-04 13:28:04 +0000

    Add a button to reset the preview link
    This posts to the controller action in the previous commit.

--- features/step_definitions/fact_check_link_steps.rb.rb
+++ features/step_definitions/fact_check_link_steps.rb.rb
@@ -21,3 +21,17 @@ Then(/^the link should be copied to my clipboard$/) do
   clip_text = page.evaluate_async_script("navigator.clipboard.readText().then(arguments[0])")
   expect(clip_text).to eq(@fact_check_url)
 end
+
+When(/^I click to reset the preview link$/) do
+  click_on "Reset preview link"
+end
+
+Then(/^a new preview link should be generated$/) do
+  extend FactCheckHelper
+
+  expect(page).to_not have_selector("input[value='#{@fact_check_url}']")
+  @new_fact_check_url = fact_check_url_with_token(@content_block.reload)
+  expect(@new_fact_check_url).not_to eq(@fact_check_url)
+  expect(page).to have_selector("input[value='#{@new_fact_check_url}']")
+  @fact_check_url = @new_fact_check_url
+end
