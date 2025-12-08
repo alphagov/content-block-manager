bb4e5dd49224db57e0e8aead59c684372c3b9f3e
Ed Davey <ed.davey@digital.cabinet-office.gov.uk>
2025-12-08 17:12:49 +0000

    Nest transition examples in contexts for greater clarity
    Our specification now looks like:
    
    Editions::StatusTransitionsController
      #create
        when the valid transition is '#publish'
          retrieves the given edition
          attempts to transition the given edition to the relevant state
          when the transition succeeds
            ..
        when the valid transition is '#ready_for_review'
          retrieves the given edition
          attempts to transition the given edition to the relevant state
          when the transition succeeds
            ...
        when the valid transition is '#delete'
          retrieves the given edition
          attempts to transition the given edition to the relevant state
          when the transition succeeds
            ...

--- spec/unit/app/controllers/editions/status_transitions_controller_spec.rb
+++ spec/unit/app/controllers/editions/status_transitions_controller_spec.rb
@@ -21,38 +21,40 @@ RSpec.describe Editions::StatusTransitionsController, type: :controller do
   describe "#create" do
     # TODO: :schedule requires validation and we should handle it here if we want to use this controller for scheduling
     Edition.new.available_transitions.reject { |tr| tr == :schedule }.each do |transition|
-      transition_method = "#{transition}!"
+      context "when the valid transition is '#{transition}'" do
+        transition_method = "#{transition}!"
 
-      it "retrieves the given edition" do
-        post :create, params: { id: 123, transition: transition }
-
-        expect(Edition).to have_received(:find).with("123")
-      end
-
-      it "attempts to transition the given edition to the relevant state" do
-        allow(edition).to receive(transition_method)
-        post :create, params: { id: 123, transition: transition }
-
-        expect(edition).to have_received(transition_method)
-      end
-
-      context "when the transition succeeds" do
-        it "redirects to the correct page" do
+        it "retrieves the given edition" do
           post :create, params: { id: 123, transition: transition }
 
-          if transition == :delete
-            expect(response).to redirect_to(root_path)
-          else
-            expect(response).to redirect_to(document_path(edition.document))
-          end
+          expect(Edition).to have_received(:find).with("123")
         end
 
-        it "shows a success message" do
-          expected_success_message = "Edition has been moved into state '#{transition_state_outcome[transition]}'"
-
+        it "attempts to transition the given edition to the relevant state" do
+          allow(edition).to receive(transition_method)
           post :create, params: { id: 123, transition: transition }
 
-          expect(flash.notice).to eq(expected_success_message)
+          expect(edition).to have_received(transition_method)
+        end
+
+        context "when the transition succeeds" do
+          it "redirects to the correct page" do
+            post :create, params: { id: 123, transition: transition }
+
+            if transition == :delete
+              expect(response).to redirect_to(root_path)
+            else
+              expect(response).to redirect_to(document_path(edition.document))
+            end
+          end
+
+          it "shows a success message" do
+            expected_success_message = "Edition has been moved into state '#{transition_state_outcome[transition]}'"
+
+            post :create, params: { id: 123, transition: transition }
+
+            expect(flash.notice).to eq(expected_success_message)
+          end
         end
       end
     end
