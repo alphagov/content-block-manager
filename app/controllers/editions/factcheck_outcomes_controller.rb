9cc24398c734bd20404cd8c0c8b0a5d29a67db9b
Graham MacGregor <graham.macgregor@digital.cabinet-office.gov.uk>
2025-12-11 14:14:50 +0000

    Add create route for factcheck outcomes
    Allows user to submit the form and set the related values on the
    Edition. Handles invalid responses from the user.
    
    Doesn't yet attempt to transition the Edition to the next state.

--- app/controllers/editions/factcheck_outcomes_controller.rb
+++ app/controllers/editions/factcheck_outcomes_controller.rb
@@ -4,4 +4,42 @@ class Editions::FactcheckOutcomesController < BaseController
     @title = "Publish block"
     render :new
   end
+
+  def create
+    @edition = Edition.find(params[:id])
+    return form_validation_error unless factcheck_outcome_supplied?
+
+    record_factcheck_outcome
+  end
+
+private
+
+  def form_validation_error
+    alert = "Indicate whether the Factcheck process has been performed or not"
+    redirect_to new_factcheck_outcome_path(@edition), alert:
+  end
+
+  def record_factcheck_outcome
+    @edition.update(
+      "factcheck_skipped" => factcheck_skipped?,
+      "factcheck_outcome_recorded_at" => Time.current,
+      "factcheck_outcome_recorded_by" => Current.user.id,
+    )
+  end
+
+  def factcheck_outcome_supplied?
+    outcome_params["factcheck_performed"].present?
+  rescue ActionController::ParameterMissing
+    false
+  end
+
+  def factcheck_skipped?
+    ActiveModel::Type::Boolean.new.cast(
+      outcome_params["factcheck_performed"],
+    ) == false
+  end
+
+  def outcome_params
+    params.require("factcheck_outcome")
+  end
 end
