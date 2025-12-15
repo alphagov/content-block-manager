ed56ed4429aec67e6caddf63f0b5f22413636381
Ed Davey <ed.davey@digital.cabinet-office.gov.uk>
2025-12-15 10:45:09 +0000

    Move helpers into component
    Now that we've got a component to handle our workflow review
    actions we no longer need a "workflow_helper" to house our
    convenience methods.

--- app/components/edition/workflow/review_actions_component.rb
+++ app/components/edition/workflow/review_actions_component.rb
@@ -4,4 +4,14 @@ class Edition::Workflow::ReviewActionsComponent < ViewComponent::Base
   end
 
   delegate :pre_release_features?, to: :helpers
+
+  def button_text
+    return "Schedule" if @edition.is_scheduling?
+
+    @edition.document.is_new_block? ? "Create" : "Publish"
+  end
+
+  def save_action
+    @edition.is_scheduling? ? "schedule" : "publish"
+  end
 end
