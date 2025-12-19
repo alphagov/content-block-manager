97aff83b1ff6b9071bf2c60311bd3c38d998a221
pezholio <pezholio@gmail.com>
2025-12-19 12:19:16 +0000

    Delete obsolete translation helpers
    We no longer need this code

--- app/helpers/translation_helper.rb
+++ app/helpers/translation_helper.rb
@@ -1,12 +1,4 @@
 module TranslationHelper
-  def humanized_label(schema_name:, relative_key:, root_object: nil)
-    humanized_translation(schema_name:, relative_key:, root_object:, scope: "labels")
-  end
-
-  def humanized_title(schema_name:, relative_key:, root_object: nil)
-    humanized_translation(schema_name:, relative_key:, root_object:, scope: "titles")
-  end
-
   def translated_value(key, value)
     default_path = "edition.values.#{value}"
     translation_path = "edition.values.#{key}.#{value}"
@@ -16,30 +8,5 @@ module TranslationHelper
 
   def label_for_title(block_type)
     I18n.t("activerecord.attributes.edition/document.title.#{block_type}", default: nil) || I18n.t("activerecord.attributes.edition/document.title.default")
-  end
-
-  def hint_text(schema:, subschema:, field:)
-    translation_lookup = [
-      schema.block_type,
-      subschema&.block_type,
-      field.name,
-    ].compact.join(".")
-
-    I18n.t("edition.hints.#{translation_lookup}", default: nil)
-  end
-
-private
-
-  def humanized_translation(schema_name:, relative_key:, root_object:, scope:)
-    translation_path = [
-      schema_name,
-      root_object,
-      relative_key,
-    ].compact.join(".")
-
-    I18n.t(
-      "edition.#{scope}.#{translation_path}",
-      default: relative_key.humanize.gsub("-", " "),
-    )
   end
 end
--- spec/unit/app/helpers/summary_list_helper_spec.rb
+++ spec/unit/app/helpers/summary_list_helper_spec.rb
@@ -58,40 +58,4 @@ RSpec.describe SummaryListHelper do
       expect(expected).to eq(nested_items(input))
     end
   end
-
-  describe "#key_to_label" do
-    it "returns a titlelized version of a key without an index" do
-      expect("Item").to eq(key_to_label("item", "schema_name"))
-    end
-
-    it "returns a titleized version with a count when an index is present" do
-      expect("Item 2").to eq(key_to_label("items/1", "schema_name"))
-    end
-
-    describe "when there is a translation for the key" do
-      it "returns translated key" do
-        expect(I18n).to receive(:t).with("edition.labels.schema_name.object_type.item", default: "Item").and_return("Item translated")
-
-        expect("Item translated").to eq(key_to_label("item", "schema_name", "object_type"))
-      end
-    end
-  end
-
-  describe "#key_to_title" do
-    it "returns a titlelized version of a key without an index" do
-      expect("Item").to eq(key_to_title("item", "schema_name"))
-    end
-
-    it "returns a titleized version with a count when an index is present" do
-      expect("Item 2").to eq(key_to_title("items/1", "schema_name"))
-    end
-
-    describe "when there is a translation for the key" do
-      it "returns translated key" do
-        expect(I18n).to receive(:t).with("edition.titles.schema_name.object_type.item", default: "Item").and_return("Item translated")
-
-        expect("Item translated").to eq(key_to_title("item", "schema_name", "object_type"))
-      end
-    end
-  end
 end
