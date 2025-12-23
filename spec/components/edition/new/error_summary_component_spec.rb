706df117bb6916443492648aa2cf750d9347e6c4
Graham MacGregor <graham.macgregor@digital.cabinet-office.gov.uk>
2025-12-23 13:35:54 +0000

    convert tests in test/components/edition/new/

--- spec/components/edition/new/error_summary_component_spec.rb
+++ spec/components/edition/new/error_summary_component_spec.rb
@@ -1,10 +1,6 @@
-require "test_helper"
-
-class Edition::New::ErrorSummaryComponentTest < ViewComponent::TestCase
-  extend Minitest::Spec::DSL
-
+RSpec.describe Edition::New::ErrorSummaryComponent, type: :component do
   let(:component) do
-    Edition::New::ErrorSummaryComponent.new(error_message:)
+    described_class.new(error_message:)
   end
 
   describe "when a message is present" do
@@ -13,8 +9,8 @@ class Edition::New::ErrorSummaryComponentTest < ViewComponent::TestCase
     it "renders an error summary" do
       render_inline(component)
 
-      assert_selector ".govuk-error-summary"
-      assert_selector ".gem-c-error-summary__list-item", text: error_message
+      expect(page).to have_css ".govuk-error-summary"
+      expect(page).to have_css ".gem-c-error-summary__list-item", text: error_message
     end
   end
 
@@ -24,7 +20,7 @@ class Edition::New::ErrorSummaryComponentTest < ViewComponent::TestCase
     it "does not render a summary" do
       render_inline(component)
 
-      assert_no_selector ".govuk-error-summary"
+      expect(page).not_to have_css ".govuk-error-summary"
     end
   end
 end
