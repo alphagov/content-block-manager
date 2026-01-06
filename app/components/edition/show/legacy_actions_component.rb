10e4dc83b874227866eb9d7975aeb75ad40b8bbd
Graham MacGregor <graham.macgregor@digital.cabinet-office.gov.uk>
2026-01-06 13:54:48 +0000

    Only create new draft if none already exist
    This also introduces a link back to the latest edition when viewing
    the published edition (and the document has a more recent draft).
    
    This mirrors the logic we have in the actions component but will be
    active for our users who don't have the pre_release_features flag
    set.
    
    I've put this into its own component so that when we're ready to
    release these changes we can simply delete this 'legacy' component.

---
+++ app/components/edition/show/legacy_actions_component.rb
@@ -0,0 +1,19 @@
+class Edition::Show::LegacyActionsComponent < ViewComponent::Base
+  def initialize(edition:)
+    @edition = edition
+    @state = edition.state
+    @document = edition.document
+  end
+
+  attr_reader :edition, :state, :document
+
+  delegate :completed?, to: :edition
+
+  def finalised_state?
+    state.to_sym.in?(Edition.finalised_states)
+  end
+
+  def has_more_recent_draft?
+    document.most_recent_edition&.id != edition.id
+  end
+end
---
+++ spec/components/edition/show/legacy_actions_component_spec.rb
@@ -0,0 +1,224 @@
+RSpec.describe Edition::Show::LegacyActionsComponent, type: :component do
+  let(:document) { FactoryBot.build(:document, :pension, id: 456) }
+  let(:edition) { FactoryBot.build(:edition, :pension, document: document, id: 123) }
+
+  describe "link to create new draft edition" do
+    context "for 'in-progress' states" do
+      Edition.in_progress_states.each do |state|
+        context "when the edition is in the '#{state}' state" do
+          before do
+            edition.state = state
+            component = described_class.new(edition: edition)
+            render_inline component
+          end
+
+          it "does NOT offer an 'Edit {block type}' link to create a new draft edition" do
+            expect(page).to have_no_css(
+              ".actions a.govuk-button[href='/456/editions/new']",
+              text: "Edit pension",
+            )
+          end
+        end
+      end
+    end
+
+    context "for 'finalised' states" do
+      let(:latest_edition_id) { 123 }
+      let(:newer_draft_edition_id) { 124 }
+
+      Edition.finalised_states.each do |state|
+        context "when the edition is in the '#{state}' state" do
+          before do
+            edition.state = state
+          end
+
+          context "when the edition being viewed is not the latest edition" do
+            before do
+              allow(document).to receive(:most_recent_edition).and_return(double(:edition, id: newer_draft_edition_id))
+              component = described_class.new(edition: edition)
+              render_inline component
+            end
+
+            it "offers an 'Edit latest edition' link to return to editing the draft edition" do
+              expect(page).to have_css(
+                ".actions a.govuk-button[href='/editions/123/workflow/review']",
+                text: "Edit latest edition",
+              )
+            end
+          end
+
+          context "when the edition being viewed **is** the latest edition" do
+            before do
+              allow(document).to receive(:most_recent_edition).and_return(double(:edition, id: latest_edition_id))
+              component = described_class.new(edition: edition)
+              render_inline component
+            end
+
+            it "offers an 'Edit {block type}' link to create a new draft edition" do
+              expect(page).to have_css(
+                ".actions a.govuk-button[href='/456/editions/new']",
+                text: "Edit pension",
+              )
+            end
+          end
+        end
+      end
+    end
+  end
+
+  describe "link to 'Edit draft'" do
+    before { allow(edition).to receive(:completed?).and_return(true) }
+
+    context "for 'in-progress' states" do
+      Edition.in_progress_states.each do |state|
+        context "when the edition is in the '#{state}' state" do
+          before do
+            edition.state = state
+            component = described_class.new(edition: edition)
+            render_inline component
+          end
+
+          it "offers a secondary 'Edit draft' link to edit the current draft" do
+            expect(page).to have_css(
+              ".actions a.govuk-button--secondary[href='/editions/123/workflow/review']",
+              text: "Edit draft",
+            )
+          end
+        end
+      end
+    end
+
+    context "when the edition is in the 'draft' state but not completed" do
+      before do
+        allow(edition).to receive(:completed?).and_return(false)
+        edition.state = "draft"
+        component = described_class.new(edition: edition)
+        render_inline component
+      end
+
+      it "offers a 'Complete draft' link to edit the current draft" do
+        expect(page).to have_css(
+          ".actions a.govuk-button[href='/editions/123/workflow/review']",
+          text: "Complete draft",
+        )
+      end
+    end
+
+    context "for 'finalised' states" do
+      Edition.finalised_states.each do |state|
+        context "when the edition is in the '#{state}' state" do
+          before do
+            edition.state = state
+            component = described_class.new(edition: edition)
+            render_inline component
+          end
+
+          it "does NOT offer an 'Edit draft' link to edit a current draft" do
+            expect(page).to have_no_css(
+              ".actions a.govuk-button[href='/editions/123/workflow/review']",
+              text: "Edit draft",
+            )
+          end
+        end
+      end
+    end
+  end
+
+  describe "link to latest published edition" do
+    context "when the edition is in the 'published' state" do
+      before do
+        edition.state = :published
+        component = described_class.new(edition: edition)
+        render_inline component
+      end
+
+      it "does NOT offer the link to the published edition" do
+        expect(page).to have_no_link("Go to published edition")
+      end
+    end
+
+    (Edition.available_states - [:published]).each do |state|
+      context "when the edition is in the '#{state}' state" do
+        before do
+          edition.state = state
+        end
+
+        context "if a published edition exists" do
+          before do
+            allow(document).to receive(:has_published_edition?).and_return(true)
+            component = described_class.new(edition: edition)
+            render_inline component
+          end
+
+          it "offers the link to the published edition" do
+            expect(page).to have_link(
+              "Go to published edition",
+              href: "/456/published_edition",
+            )
+          end
+        end
+
+        context "if a published edition does NOT exist" do
+          before do
+            allow(document).to receive(:has_published_edition?).and_return(false)
+            component = described_class.new(edition: edition)
+            render_inline component
+          end
+
+          it "does NOT offer a link to the published edition" do
+            expect(page).to have_no_link("Go to published edition")
+          end
+        end
+      end
+    end
+  end
+
+  describe "link to go back to the latest edition" do
+    context "when the edition being viewed is published" do
+      let(:latest_edition_id) { 123 }
+      let(:newer_draft_edition_id) { 124 }
+
+      before do
+        edition.state = :published
+      end
+
+      context "and the edition being viewed is not the latest edition" do
+        before do
+          allow(document).to receive(:most_recent_edition).and_return(double(:edition, id: newer_draft_edition_id))
+          component = described_class.new(edition: edition)
+          render_inline component
+        end
+
+        it "shows the link to return to the latest edition" do
+          expect(page).to have_link("Return to latest edition", href: "/456")
+        end
+      end
+
+      context "and the edition being viewed is the latest edition" do
+        before do
+          allow(document).to receive(:most_recent_edition).and_return(double(:edition, id: latest_edition_id))
+          component = described_class.new(edition: edition)
+          render_inline component
+        end
+
+        it "shows the link to return to the latest edition" do
+          expect(page).to have_no_link("Return to latest edition", href: "/456")
+        end
+      end
+    end
+
+    (Edition.available_states - [:published]).each do |state|
+      context "when the edition being viewed is not published (#{state})" do
+        before do
+          edition.state = state
+          component = described_class.new(edition: edition)
+          render_inline component
+        end
+
+        it "does not show the link to return to the latest edition" do
+          expect(page).to have_no_link("Return to latest edition")
+        end
+      end
+    end
+  end
+end
