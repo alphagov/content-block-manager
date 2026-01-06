9c70cb9b7dd6edd2152dfd7781ee4c9376856344
pezholio <pezholio@gmail.com>
2026-01-06 10:44:43 +0000

    Use context in StringComponent
    This also adds some of the core behaviour for a field into a shared
    example, which will allow us to simplify a lot of these tests

--- app/components/edition/details/fields/string_component.rb
+++ app/components/edition/details/fields/string_component.rb
@@ -1,2 +1,11 @@
-class Edition::Details::Fields::StringComponent < Edition::Details::Fields::BaseComponent
+class Edition::Details::Fields::StringComponent < ViewComponent::Base
+  def initialize(context)
+    @context = context
+  end
+
+private
+
+  attr_reader :context
+
+  delegate :label, :name, :id, :value, :error_items, :hint_text, to: :context
 end
