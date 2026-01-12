1207da1487563ffd281a70760c6ef1f788d1db1f
Graham MacGregor <graham.macgregor@digital.cabinet-office.gov.uk>
2026-01-12 14:04:56 +0000

    Add cucumber debug flag to open page on failure

--- features/support/debug.rb
+++ features/support/debug.rb
@@ -2,4 +2,10 @@
 When("I save and open the page") do
   save_and_open_page
 end
+
+After do |scenario|
+  if scenario.failed? && ENV["DEBUG_OPEN_ON_FAILURE"] == "true"
+    save_and_open_page
+  end
+end
 # rubocop:enable Lint/Debugger
