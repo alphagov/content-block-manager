a48bb4d33eacc8b600c700b6fd2c73aa47471ac4
Ed Davey <ed.davey@digital.cabinet-office.gov.uk>
2026-01-14 21:20:52 +0000

    Render embed code in DefaultBlock on published state
    As per the more detailed spec, the new behaviour is:
    
    Edition::Show::DefaultBlockComponent
    
    when the edition is in the 'published' state:
      - includes the default block output in a govspeak enabled element
      - includes the embed code in its own element
      - includes 'data' attributes to initialise the CopyEmbedCode JS module
    
    when the edition is in any non-published state:
      - includes the default block output in a govspeak enabled element
      - does NOT include the embed code in its own element
      - does NOT include 'data' attributes to initialise the CopyEmbedCode JS module

--- spec/components/edition/show/default_block_component_spec.rb
+++ spec/components/edition/show/default_block_component_spec.rb
@@ -11,36 +11,64 @@ RSpec.describe Edition::Show::DefaultBlockComponent, type: :component do
     allow(edition).to receive(:render).with(embed_code).and_return(default_block_output)
   end
 
-  it "includes the default block output in a govspeak enabled element" do
-    render_inline(
-      Edition::Show::DefaultBlockComponent.new(edition: edition),
-    )
+  context "when the edition is in the 'published' state" do
+    before do
+      edition.state = :published
+      render_inline(Edition::Show::DefaultBlockComponent.new(edition: edition))
+    end
 
-    expect(page).to have_css(
-      ".govuk-summary-list__row[data-module=\"copy-embed-code\"][data-embed-code=\"#{embed_code}\"] .govuk-summary-list__value .govspeak",
-      text: default_block_output,
-    )
+    it "includes the default block output in a govspeak enabled element" do
+      expect(page).to have_css(
+        ".govspeak",
+        text: default_block_output,
+      )
+    end
+
+    it "includes the embed code in its own element" do
+      expect(page).to have_css(
+        ".govuk-summary-list__value .app-c-content-block-manager-default-block__embed_code",
+        text: embed_code,
+      )
+    end
+
+    it "includes 'data' attributes to initialise the CopyEmbedCode JS module" do
+      data_attrs = [
+        "[data-module='copy-embed-code']",
+        "[data-embed-code='EMBED_CODE']",
+        "[data-embed-code-details='default block']",
+      ]
+
+      expect(page).to have_css("div#{data_attrs.join}")
+    end
   end
 
-  it "includes the embed code in its own element" do
-    render_inline(
-      Edition::Show::DefaultBlockComponent.new(edition: edition),
-    )
+  (Edition.available_states - [:published]).each do |state|
+    context "when the edition is in a non-published state (#{state})" do
+      before do
+        edition.state = state
+        render_inline(Edition::Show::DefaultBlockComponent.new(edition: edition))
+      end
 
-    expect(page).to have_css(
-      ".govuk-summary-list__value .app-c-content-block-manager-default-block__embed_code",
-      text: embed_code,
-    )
-  end
+      it "includes the default block output in a govspeak enabled element" do
+        expect(page).to have_css(
+          ".govspeak",
+          text: default_block_output,
+        )
+      end
 
-  it "includes detail on embed code in a data attr for use in visually hidden link info" do
-    render_inline(
-      Edition::Show::DefaultBlockComponent.new(edition: edition),
-    )
+      it "does NOT include the embed code in its own element" do
+        expect(page).not_to have_content(embed_code)
+      end
 
-    expect(page).to have_css(
-      ".govuk-summary-list__row[data-embed-code-details='#{embed_code_details}']",
-      text: default_block_output,
-    )
+      it "does NOT include 'data' attributes to initialise the CopyEmbedCode JS module" do
+        data_attrs = [
+          "[data-module='copy-embed-code']",
+          "[data-embed-code='EMBED_CODE']",
+          "[data-embed-code-details='default block']",
+        ]
+
+        expect(page).to have_no_css("div#{data_attrs.join}")
+      end
+    end
   end
 end
