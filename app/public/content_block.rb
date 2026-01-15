44dbac804f76a1e96e81019aba9ad4eab7bea36d
Stacy Hayes <stacy.hayes@digital.cabinet-office.gov.uk>
2026-01-15 15:47:06 +0000

    Render state on factcheck page
    The designs include a state label for the edition in factcheck which is the
    same as the state label on the standard show page, this just adds that component
    to the factcheck show

--- app/public/content_block.rb
+++ app/public/content_block.rb
@@ -11,7 +11,7 @@ class ContentBlock
     @edition = edition
   end
 
-  delegate :title, :auth_bypass_id, :content_id, to: :edition
+  delegate :title, :state, :auth_bypass_id, :content_id, to: :edition
 
   def block_type
     edition.document.schema.name
