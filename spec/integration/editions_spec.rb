2596521d61d0e859d333cc7379db40cb219562b4
Graham MacGregor <graham.macgregor@digital.cabinet-office.gov.uk>
2026-01-16 09:34:51 +0000

    Refactor edition spec file to use more common syntax / format
    The format of this file didn't seem to match what I usually see in our
    RSpec files. I assume this is a hangover from minitest testing styles.

--- spec/integration/editions_spec.rb
+++ spec/integration/editions_spec.rb
@@ -1,5 +1,3 @@
-require "capybara/rails"
-
 RSpec.describe Editions, type: :feature do
   include Rails.application.routes.url_helpers
 
@@ -17,41 +15,62 @@ RSpec.describe Editions, type: :feature do
     describe "when a document id is provided" do
       let!(:original_edition) { create(:edition, :pension, document: document) }
       let(:document) { create(:document, :pension) }
+      let(:schema) { stub_request_for_schema(document.block_type) }
+      let(:form) do
+        double(:form,
+               title: "title",
+               url: "url",
+               back_path: "back_path",
+               edition: original_edition,
+               schema:,
+               attributes: {},
+               form_method: :post)
+      end
 
       before do
         document.latest_published_edition = original_edition
         document.save!
-      end
 
-      scenario "initializes the form for the latest edition" do
-        expect(Document).to receive(:find).with(document.id.to_s).and_return(document)
-        schema = stub_request_for_schema(document.block_type)
-        form = double(:form, title: "title", url: "url", back_path: "back_path", edition: original_edition, schema:, attributes: {}, form_method: :post)
-        expect(EditionForm).to receive(:for).with(
+        allow(Document).to receive(:find).with(document.id.to_s).and_return(document)
+        allow(EditionForm).to receive(:for).with(
           edition: original_edition,
           schema:,
         ).and_return(form)
+      end
 
+      it "initializes the form for the latest edition" do
         visit new_document_edition_path(document)
 
-        assert_text document.title
+        expect(page).to have_content(document.title)
       end
     end
 
     describe "when a document id is not provided" do
-      scenario "initializes the form for the latest edition" do
-        edition = create(:edition, :pension)
-        expect(Edition).to receive(:new).and_return(edition)
-        schema = stub_request_for_schema("block_type")
-        form = double(:form, title: "title", url: "url", back_path: "back_path", edition: edition, schema:, attributes: {}, form_method: :post)
-        expect(EditionForm).to receive(:for).with(
+      let(:edition) { create(:edition, :pension) }
+      let(:schema) { stub_request_for_schema("block_type") }
+      let(:form) do
+        double(:form,
+               title: "title",
+               url: "url",
+               back_path: "back_path",
+               edition: edition,
+               schema:,
+               attributes: {},
+               form_method: :post)
+      end
+
+      before do
+        allow(Edition).to receive(:new).and_return(edition)
+        allow(EditionForm).to receive(:for).with(
           edition: edition,
           schema:,
         ).and_return(form)
+      end
 
+      it "initializes the form for the latest edition" do
         visit new_edition_path(block_type: "block-type")
 
-        assert_text "Create content block"
+        expect(page).to have_content("Create content block")
       end
     end
   end
@@ -68,24 +87,24 @@ RSpec.describe Editions, type: :feature do
       allow(edition).to receive(:has_multiple_subschema_entries?).and_return(true)
     end
 
-    scenario "renders a preview of the edition" do
+    it "renders a preview of the edition" do
       visit preview_edition_path(edition)
 
       expect(page).to have_css ".app-views-editions-preview .govspeak", text: "RENDERED_BLOCK"
     end
 
-    scenario "shows a link to reorder if has_multiple_subschema_entries? is true" do
+    it "shows a link to reorder if has_multiple_subschema_entries? is true" do
       allow(edition).to receive(:has_multiple_subschema_entries?).and_return(true)
       visit preview_edition_path(edition)
 
       expect(page).to have_css "a.govuk-button", text: "Reorder"
     end
 
-    scenario "shows NO link to reorder if has_multiple_subschema_entries? is false" do
+    it "shows NO link to reorder if has_multiple_subschema_entries? is false" do
       allow(edition).to receive(:has_multiple_subschema_entries?).and_return(false)
       visit preview_edition_path(edition)
 
-      assert_no_selector "a.govuk-button", text: "Reorder"
+      expect(page).not_to have_css("a.govuk-button", text: "Reorder")
     end
   end
 end
