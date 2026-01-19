2f6dc8a586a3a138cc24aa042fadded0ffbe910e
Ed Davey <ed.davey@digital.cabinet-office.gov.uk>
2026-01-19 17:45:00 +0000

    Refactor for clarity

--- app/models/edition/state_transition_message.rb
+++ app/models/edition/state_transition_message.rb
@@ -5,14 +5,16 @@ class Edition::StateTransitionMessage
   end
 
   def to_s
-    if edition.first_edition?
-      I18n.t("edition.states.transition_message.first.#{state}")
-    else
-      I18n.t("edition.states.transition_message.further.#{state}")
-    end
+    I18n.t("edition.states.transition_message.#{first_or_further}.#{state}")
   end
 
 private
 
   attr_reader :edition, :state
+
+  def first_or_further
+    return "first" if edition.first_edition?
+
+    "further"
+  end
 end
