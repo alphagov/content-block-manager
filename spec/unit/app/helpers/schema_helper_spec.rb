2a4d3819572d1264f538f516f5a3a6ca8a24a54c
Graham MacGregor <graham.macgregor@digital.cabinet-office.gov.uk>
2026-01-23 13:49:37 +0000

    Add new SubschemaCollection class to handle subschemas
    We had these 'grouped' and 'ungrouped' methods as global helpers. For
    a few reasons I want to move these to be part of a more structured
    approach.
    
    1. I want to use these in the Fact Check engine and helpers aren't as
    easily shared.
    2. Moving these out of the global namespace and into a more
    intentional pattern.
    
    I initially thought about putting these into the Schema model but it
    already seems overloaded with methods so I'm treating these as their
    own thing. There may be one or two Schema methods we could move here
    in future too.

--- spec/unit/app/helpers/schema_helper_spec.rb
+++ spec/unit/app/helpers/schema_helper_spec.rb
@@ -1,49 +1,6 @@
 RSpec.describe SchemaHelper, type: :helper do
   include Rails.application.routes.url_helpers
 
-  let(:schema) { double(:schema) }
-
-  let(:group_1_subschemas) do
-    [
-      double(:subschema, group: "group_1"),
-      double(:subschema, group: "group_1"),
-    ]
-  end
-
-  let(:group_2_subschemas) do
-    [
-      double(:subschema, group: "group_2"),
-      double(:subschema, group: "group_2"),
-      double(:subschema, group: "group_2"),
-    ]
-  end
-
-  let(:subschemas_without_groups) do
-    [
-      double(:subschema, group: nil),
-      double(:subschema, group: nil),
-      double(:subschema, group: nil),
-      double(:subschema, group: nil),
-      double(:subschema, group: nil),
-    ]
-  end
-
-  before do
-    allow(schema).to receive(:subschemas).and_return([*group_1_subschemas, *group_2_subschemas, *subschemas_without_groups])
-  end
-
-  describe "#grouped_subschemas" do
-    it "returns all grouped subschemas" do
-      expect({ "group_1" => group_1_subschemas, "group_2" => group_2_subschemas }).to eq(grouped_subschemas(schema))
-    end
-  end
-
-  describe "#ungrouped_subschemas" do
-    it "returns all ungrouped subschemas" do
-      expect(subschemas_without_groups).to eq(ungrouped_subschemas(schema))
-    end
-  end
-
   describe "#redirect_url_for_subschema" do
     let(:edition) { build_stubbed(:edition, :contact) }
     let(:subschema) { double(:subschema, group:, id: "my_subschema") }
