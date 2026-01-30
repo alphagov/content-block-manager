5240c1074882a55c172a1ce5a88dae693ab940bd
pezholio <pezholio@gmail.com>
2026-01-30 09:29:40 +0000

    Update GA4 steps
    Now the steps include JSON, we need to update some of the expectations.
    I’ve also tidied and DRYed up some of the code, so we have a single
    `assert_messages_sent` helper

--- features/step_definitions/ga4_steps.rb.rb
+++ features/step_definitions/ga4_steps.rb.rb
@@ -1,11 +1,6 @@
 Then("analytics messages should have been sent for each step in the workflow") do
-  edition = Edition.last
-  steps = Workflow::Steps.for(edition, @schema)
-
-  steps.each do |step|
-    assert ga4_messages.any? do |message|
-      message["event_name"] == @action && message["section"] == step
-    end
+  all_steps_for_edition(Edition.last).each do |step|
+    assert_messages_sent(event_name: @action, section: step.to_s)
   end
 end
 
@@ -13,8 +8,8 @@ Then("analytics messages should have been sent for each embedded object") do
   extend SchemaHelper
 
   @selected_subschemas.each do |subschema|
-    assert select_subschema_messages_sent_for_subschema?(subschema) if grouped_subschemas(@schema).any?
-    assert add_subschema_messages_sent_for_subschema?(subschema)
+    expect_select_subschema_messages_to_have_been_sent(subschema) if grouped_subschemas(@schema).any?
+    expect_add_subschema_messages_to_have_been_sent(subschema)
   end
 end
 
@@ -33,23 +28,26 @@ rescue JSON::ParserError
   {}
 end
 
-def select_schema_messages_sent?
-  assert_equal ga4_messages[0]["event_name"], @action
-  assert_equal ga4_messages[0]["section"], "select_schema"
+def assert_messages_sent(**args)
+  expected_messages = args.to_h.stringify_keys
+  expect(ga4_messages).to include(include(expected_messages))
 end
 
-def select_subschema_messages_sent_for_subschema?(subschema)
-  ga4_messages.any? do |message|
-    message["event_name"] == @action &&
-      message["section"] == "select_subschema" &&
-      message["text"] == @schema.subschema(subschema).name.singularize
-  end
+def expect_select_subschema_messages_to_have_been_sent(subschema)
+  expected_text = {
+    @schema.subschema(subschema).name.singularize.capitalize => @schema.subschema(subschema).name.singularize.capitalize,
+  }.to_json
+  assert_messages_sent(event_name: @action, section: "select_subschema", text: expected_text)
 end
 
-def add_subschema_messages_sent_for_subschema?(subschema)
-  ga4_messages.any? do |message|
-    message["event_name"] == @action &&
-      message["section"] == "add_#{subschema}" &&
-      message["tool_name"] == @schema.block_type
-  end
+def expect_add_subschema_messages_to_have_been_sent(subschema)
+  assert_messages_sent(event_name: @action, section: "add_#{subschema}", tool_name: @schema.block_type)
+end
+
+# Fetch all steps for an edition and schema, except the edit step, which is only accessed when making changes during
+# the edit/create flow and the last step, which does not accept a form submission
+def all_steps_for_edition(edition)
+  Workflow::Steps.for(edition, @schema)
+                 .map(&:name)
+                 .excluding(:edit_draft, :confirmation)
 end
