bdb9f093abf66fc1e06bb8b41b59e5688e0a4b89
Graham MacGregor <graham.macgregor@digital.cabinet-office.gov.uk>
2026-02-06 12:18:47 +0000

    Add TabGroupDiffComponent

---
+++ engines/fact_check/app/components/fact_check/tab_group_diff_component.rb
@@ -0,0 +1,34 @@
+class FactCheck::TabGroupDiffComponent < ViewComponent::Base
+  def initialize(edition:, schema:, subschemas:)
+    @edition = edition
+    @schema = schema
+    @subschemas = subschemas
+  end
+
+private
+
+  attr_reader :edition, :schema, :subschemas
+
+  def tabs
+    subschemas.sort_by(&:group_order).map do |subschema|
+      tab_for_subschema(subschema)
+    end
+  end
+
+  def tab_for_subschema(subschema)
+    component = component_for_subschema(subschema)
+    {
+      id: component.id,
+      label: component.label,
+      content: render(component),
+    }
+  end
+
+  def component_for_subschema(subschema)
+    FactCheck::SubschemaGroupDiffComponent.new(
+      edition:,
+      schema:,
+      subschema:,
+    )
+  end
+end
