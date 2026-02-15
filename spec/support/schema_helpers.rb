5ce1718359edaaf8da182e9382e98f6440a1d464
Graham MacGregor <graham.macgregor@digital.cabinet-office.gov.uk>
2026-02-15 10:43:25 +0000

    Create minimal realistic contact schema body
    This will remove a bit of the cognitive overhead from the spec file
    and allows for easy re-use if any other tests need this.
    
    This isn't a value we will ever alter in the spec files so the
    reader doesn't need to understand the whole hash, they just needs to
    understand that it's somewhat realistic and that it works.

--- spec/support/schema_helpers.rb
+++ spec/support/schema_helpers.rb
@@ -1,4 +1,18 @@
 module SchemaHelpers
+  MINIMAL_CONTACT_SCHEMA_BODY =
+    { "type" => "object",
+      "properties" =>
+      { "addresses" =>
+        { "type" => "object",
+          "patternProperties" =>
+          { ".*" =>
+            { "type" => "object",
+              "properties" =>
+              { "state_or_county" => { "type" => "string" },
+                "street_address" => { "type" => "string" },
+                "title" => { "type" => "string" },
+                "town_or_city" => { "type" => "string" } } } } } } }.freeze
+
   def stub_request_for_schema(block_type, subschemas: [], fields: nil)
     schema = double(
       id: "content_block_type",
@@ -30,4 +44,5 @@ end
 RSpec.configure do |config|
   config.include SchemaHelpers, type: :request
   config.include SchemaHelpers, type: :feature
+  config.include SchemaHelpers, type: :component
 end
