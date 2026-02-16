688b1383031449f3669c5147d134c6c09bca0dc0
Graham MacGregor <graham.macgregor@digital.cabinet-office.gov.uk>
2026-02-16 11:50:05 +0000

    Extract methods for finding removed and added content

--- engines/fact_check/features/step_definitions/fact_check_steps.rb
+++ engines/fact_check/features/step_definitions/fact_check_steps.rb
@@ -7,11 +7,11 @@ Then("I should see the block's title") do
 end
 
 Then("I should see {string} as a previous value") do |value|
-  expect(page).to have_css("[aria-label=\"removed content\"]", text: /#{value}/)
+  expect_removed_content_in_block(value)
 end
 
 Then("I should see {string} as a new value") do |value|
-  expect(page).to have_css("[aria-label=\"added content\"]", text: /#{value}/)
+  expect_added_content_in_block(value)
 end
 
 Then("I should not see a diff") do
@@ -35,11 +35,11 @@ Then("I should see a summary card entitled {string}") do |card_title|
 end
 
 Then("I should see {string} as a previous value in the summary card") do |value|
-  expect(@summary_block).to have_css("[aria-label=\"removed content\"]", text: /#{value}/)
+  expect_removed_content_in_block(value, @summary_block)
 end
 
 Then("I should see {string} as a new value in the summary card") do |value|
-  expect(@summary_block).to have_css("[aria-label=\"added content\"]", text: /#{value}/)
+  expect_added_content_in_block(value, @summary_block)
 end
 
 When("I open the full {string} summary card attributes") do |object_type|
@@ -49,13 +49,21 @@ When("I open the full {string} summary card attributes") do |object_type|
 end
 
 Then("I should see {string} as a previous value in the summary details") do |value|
-  expect(@summary_details).to have_css("[aria-label=\"removed content\"]", text: /#{value}/)
+  expect_removed_content_in_block(value, @summary_details)
 end
 
 Then("I should see {string} as a new value in the summary details") do |value|
-  expect(@summary_details).to have_css("[aria-label=\"added content\"]", text: /#{value}/)
+  expect_added_content_in_block(value, @summary_details)
 end
 
 Then("I should see {string} in the summary details") do |value|
   expect(@summary_details).to have_text(value)
+end
+
+def expect_removed_content_in_block(value, block = page)
+  expect(block).to have_css("[aria-label=\"removed content\"]", text: /#{value}/)
+end
+
+def expect_added_content_in_block(value, block = page)
+  expect(block).to have_css("[aria-label=\"added content\"]", text: /#{value}/)
 end
