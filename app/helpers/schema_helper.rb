f8b69305795bc36a1fd840c4dc28be0056ba3b09
pezholio <pezholio@gmail.com>
2026-02-17 11:13:40 +0000

    Add a helper method to return valid schemas
    We have previously had issues where we are checking what schemas
    are considered ‘live’ when the feature flag is turned off, but the
    user has permissions that negate this. If we are running code that
    DOESN’T have access to the session, then `Current.user` is not available
    to the code, so we get unexpected results.
    
    We will get a list of schemas valid for the current user via a helper
    method (which does have access to `Current.user` and use this every
    time we need to get a list of relevant block types for the logged-in
    user.

--- app/helpers/schema_helper.rb
+++ app/helpers/schema_helper.rb
@@ -3,4 +3,14 @@ module SchemaHelper
     step = subschema.group.present? ? "#{Workflow::Step::GROUP_PREFIX}#{subschema.group}" : "#{Workflow::Step::SUBSCHEMA_PREFIX}#{subschema.id}"
     workflow_path(edition, step:)
   end
+
+  def valid_schemas
+    show_all_content_block_types? ? Schema.all : Schema.live
+  end
+
+private
+
+  def show_all_content_block_types?
+    Flipflop.show_all_content_block_types? || Current.user&.has_permission?(User::Permissions::SHOW_ALL_CONTENT_BLOCK_TYPES)
+  end
 end
--- spec/unit/app/helpers/schema_helper_spec.rb
+++ spec/unit/app/helpers/schema_helper_spec.rb
@@ -21,4 +21,50 @@ RSpec.describe SchemaHelper, type: :helper do
       end
     end
   end
+
+  describe "#valid_schemas" do
+    let(:user) { build(:user) }
+    let(:user_can_view_all_content_block_types?) { false }
+
+    let(:live_schemas) { 2.times.map { double("schema") } }
+    let(:alpha_schemas) { 3.times.map { double("schema") } }
+
+    let(:all_schemas) { live_schemas + alpha_schemas }
+
+    before do
+      allow(Schema).to receive(:live).and_return(live_schemas)
+      allow(Schema).to receive(:all).and_return(all_schemas)
+
+      allow(Current).to receive(:user).and_return(user)
+      allow(user).to receive(:has_permission?)
+                       .with(User::Permissions::SHOW_ALL_CONTENT_BLOCK_TYPES)
+                       .and_return(user_can_view_all_content_block_types?)
+
+      allow(Flipflop).to receive(:show_all_content_block_types?).and_return(show_all_content_block_types_feature_flag_is_on?)
+    end
+
+    describe "when the show_all_content_block_types feature flag is turned on" do
+      let(:show_all_content_block_types_feature_flag_is_on?) { true }
+
+      it "returns all of the schemas" do
+        expect(valid_schemas).to eq(all_schemas)
+      end
+    end
+
+    describe "when the show_all_content_block_types feature flag is turned off" do
+      let(:show_all_content_block_types_feature_flag_is_on?) { false }
+
+      it "only returns the live schemas" do
+        expect(valid_schemas).to eq(live_schemas)
+      end
+
+      describe "when the current user has the show_all_content_block_types permission" do
+        let(:user_can_view_all_content_block_types?) { true }
+
+        it "returns all of the schemas" do
+          expect(valid_schemas).to eq(all_schemas)
+        end
+      end
+    end
+  end
 end
