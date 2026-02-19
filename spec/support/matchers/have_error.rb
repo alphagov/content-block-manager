0a3a2eeb9de40e6bb2d6f1e1198d48029e4fb211
pezholio <pezholio@gmail.com>
2026-02-19 14:22:01 +0000

    Add a `have_error` RSpec validator
    This is a bit more RSpec-y than our `expect_error` method and
    easier to share across specs

---
+++ spec/support/matchers/have_error.rb
@@ -0,0 +1,21 @@
+RSpec::Matchers.define :have_error_for do |key|
+  match do |model|
+    @model = model
+    @error = model.errors[key]
+    @error.present? && has_expected_message?
+  end
+
+  chain :with_error_message_for do |opts|
+    type = opts.fetch(:type)
+    attribute = opts.fetch(:attribute)
+    @expected_message = I18n.t("activerecord.errors.models.edition.#{type}", attribute:)
+  end
+
+private
+
+  def has_expected_message?
+    return true if @expected_message.nil?
+
+    @error.include?(@expected_message)
+  end
+end
