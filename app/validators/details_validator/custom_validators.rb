6c98dc7a38f50ee8d49bde70a0bae70a5b6b8487
pezholio <pezholio@gmail.com>
2026-02-19 15:13:50 +0000

    Allow `formatMinimum` to be a `$ref`
    This allows us to support, for example date ranges, where the end date
    must be after the start date.
    
    We do this by passing the body of the hash we’re validating, then
    checking if `formatMinimum` is a hash. If it is, we dig into the body
    and set `min_str` to that value.

--- app/validators/details_validator/custom_validators.rb
+++ app/validators/details_validator/custom_validators.rb
@@ -1,12 +1,17 @@
 class DetailsValidator < ActiveModel::Validator
   module CustomValidators
-    def format_date_minimum
+    def format_date_minimum(body)
       proc do |instance, schema, _instance_location|
         return true unless instance.is_a?(String)
         return true unless schema["format"] == "date"
 
         min_str = schema["formatMinimum"]
         return true if min_str.nil?
+
+        if min_str.is_a?(Hash)
+          lookup = min_str["$ref"].delete_prefix("#").split("/").compact_blank
+          min_str = body.dig(*lookup)
+        end
 
         inst = Date.iso8601(instance)
         min  = Date.iso8601(min_str)
--- spec/unit/app/validators/details_validator/custom_validators_spec.rb
+++ spec/unit/app/validators/details_validator/custom_validators_spec.rb
@@ -43,5 +43,56 @@ RSpec.describe DetailsValidator::CustomValidators do
 
       expect(edition).to be_valid
     end
+
+    describe "when formatMinimum is a pointer" do
+      let(:body) do
+        {
+          "type" => "object",
+          "additionalProperties" => false,
+          "properties" => {
+            "start_date" => {
+              "type" => "string",
+              "format" => "date",
+              "formatMinimum" => "2022-01-01",
+            },
+            "end_date" => {
+              "type" => "string",
+              "format" => "date",
+              "formatMinimum" => { "$ref" => "#/start_date" },
+            },
+          },
+        }
+      end
+
+      it "is invalid if the date is before the pointer" do
+        edition = build(
+          :edition,
+          :pension,
+          details: {
+            start_date: "2025-01-01",
+            end_date: "2024-01-01",
+          },
+          schema:,
+        )
+
+        expect(edition).to be_invalid
+
+        expect(edition).to have_error_for(:details_end_date).with_error_message_for(type: "invalid", attribute: "End date")
+      end
+
+      it "is valid if the date is after the pointer" do
+        edition = build(
+          :edition,
+          :pension,
+          details: {
+            start_date: "2025-01-01",
+            end_date: "2026-01-01",
+          },
+          schema:,
+        )
+
+        expect(edition).to be_valid
+      end
+    end
   end
 end
