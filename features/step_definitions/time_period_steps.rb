8ecdaa6ec2f5a81d0d48ee32ebfceea2c3f20fb8
Graham MacGregor <graham.macgregor@digital.cabinet-office.gov.uk>
2026-02-25 17:08:51 +0000

    Fix feature tests
    These were expecting the single text input fields we used to have so
    I've updated them to be able to handle the 5-field datetime_fields
    component instead.

--- features/step_definitions/time_period_steps.rb
+++ features/step_definitions/time_period_steps.rb
@@ -54,43 +54,47 @@ module TimePeriodHelpers
   end
 
   def fill_time_period_fields(details:, page:)
-    page.fill_in(
-      "edition_details_date_range_start_date",
-      with: details.date_range.dig("start", "date"),
-    )
-    page.fill_in(
-      "edition_details_date_range_start_time",
-      with: details.date_range.dig("start", "time"),
-    )
+    start_year, start_month, start_day = details.date_range.dig("start", "date").split("-")
+    start_hour, start_minute = details.date_range.dig("start", "time").split(":")
 
-    page.fill_in(
-      "edition_details_date_range_end_date",
-      with: details.date_range.dig("end", "date"),
-    )
-    page.fill_in(
-      "edition_details_date_range_end_time",
-      with: details.date_range.dig("end", "time"),
-    )
+    page.fill_in("start_day", with: start_day)
+    page.fill_in("start_month", with: start_month)
+    page.fill_in("start_year", with: start_year)
+
+    page.select(start_hour, from: "start_hour")
+    page.select(start_minute, from: "start_minute")
+
+    end_year, end_month, end_day = details.date_range.dig("end", "date").split("-")
+    end_hour, end_minute = details.date_range.dig("end", "time").split(":")
+
+    page.fill_in("end_day", with: end_day)
+    page.fill_in("end_month", with: end_month)
+    page.fill_in("end_year", with: end_year)
+
+    page.select(end_hour, from: "end_hour")
+    page.select(end_minute, from: "end_minute")
   end
 
   def should_see_time_period_values_in_form(details:, page:)
-    expect(page).to have_field(
-      "edition_details_date_range_start_date",
-      with: details.date_range.dig("start", "date"),
-    )
-    expect(page).to have_field(
-      "edition_details_date_range_start_time",
-      with: details.date_range.dig("start", "time"),
-    )
+    start_year, start_month, start_day = details.date_range.dig("start", "date").split("-")
+    start_hour, start_minute = details.date_range.dig("start", "time").split(":")
 
-    expect(page).to have_field(
-      "edition_details_date_range_end_date",
-      with: details.date_range.dig("end", "date"),
-    )
-    expect(page).to have_field(
-      "edition_details_date_range_end_time",
-      with: details.date_range.dig("end", "time"),
-    )
+    expect(page).to have_field("start_day", with: start_day.to_i)
+    expect(page).to have_field("start_month", with: start_month.to_i)
+    expect(page).to have_field("start_year", with: start_year.to_i)
+
+    expect(page).to have_field("start_hour", with: start_hour)
+    expect(page).to have_field("start_minute", with: start_minute)
+
+    end_year, end_month, end_day = details.date_range.dig("end", "date").split("-")
+    end_hour, end_minute = details.date_range.dig("end", "time").split(":")
+
+    expect(page).to have_field("end_day", with: end_day.to_i)
+    expect(page).to have_field("end_month", with: end_month.to_i)
+    expect(page).to have_field("end_year", with: end_year.to_i)
+
+    expect(page).to have_field("end_hour", with: end_hour)
+    expect(page).to have_field("end_minute", with: end_minute)
   end
 
   def should_see_time_period_represented_clearly(details:, page:)
