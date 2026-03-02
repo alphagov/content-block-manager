6d693f3d4d6495627cef8cf6c0910ea7404e2e1d
pezholio <pezholio@gmail.com>
2026-03-02 16:33:59 +0000

    Update the datetime component to have seperate date/time fields
    Previously the component resolved a single `DateTimeConverter` value
    object and pulled year/month/day/hour/minute from it. This required a
    valid date/time to be stored. By routing each sub-field independently
    through `field_name_for`, the component can now correctly generate Rails
    multiparameter attribute names (e.g. `start[date(1i)]`) for each nested
    field, which is required by ProcessedParams to reassemble them on
    submit.

--- app/components/edition/details/fields/date_time_component.rb
+++ app/components/edition/details/fields/date_time_component.rb
@@ -8,5 +8,21 @@ class Edition::Details::Fields::DateTimeComponent < ViewComponent::Base
     @date_time = DateTimeConverter.from_strings(date: @details["date"], time: @details["time"]).date_time
   end
 
+private
+
   attr_reader :context, :field_name, :name_prefix, :block_type, :date_time
+
+  def field_name_for(part)
+    lookup = %w[year month day hour minute]
+    field = %w[year month day].include?(part) ? date_field : time_field
+    "#{name_prefix}[#{field_name}][#{field.name}(#{lookup.index(part) + 1}i)]"
+  end
+
+  def date_field
+    @date_field ||= context.field.nested_fields.find { |f| f.format.date? }
+  end
+
+  def time_field
+    @time_field ||= context.field.nested_fields.find { |f| f.format.time? }
+  end
 end
--- spec/components/edition/details/fields/date_time_component_spec.rb
+++ spec/components/edition/details/fields/date_time_component_spec.rb
@@ -1,7 +1,22 @@
 RSpec.describe Edition::Details::Fields::DateTimeComponent, type: :component do
-  let(:schema) { build(:embedded_schema, block_type: :date_range) }
+  let(:schema_body) do
+    {
+      "properties" => {
+        "start" => {
+          "type" => "object",
+          "properties" => {
+            "date" => { "type" => "string", "format" => "date" },
+            "time" => { "type" => "string", "format" => "time" },
+          },
+        },
+      },
+    }
+  end
+
+  let(:schema) { build(:embedded_schema, block_type: :date_range, body: schema_body) }
   let(:details) { {} }
   let(:edition) { build(:edition, :time_period, details:) }
+
   let(:context) do
     Edition::Details::Fields::Context
       .new(
@@ -10,12 +25,12 @@ RSpec.describe Edition::Details::Fields::DateTimeComponent, type: :component do
         schema:,
       )
   end
+
   let(:component) { described_class.new(context) }
 
   before do
     allow(I18n).to receive(:t).and_call_original
-    allow(I18n).to receive(:t).with("edition.titles.travel_advice.start").and_return("")
-    allow(I18n).to receive(:t).with("edition.titles.travel_advice.end").and_return("")
+    allow(I18n).to receive(:t).with("edition.titles.travel_advice.start").and_return("Start date")
     render_inline(component)
   end
 
@@ -31,7 +46,7 @@ RSpec.describe Edition::Details::Fields::DateTimeComponent, type: :component do
 
   describe "the 'name' of the inputs" do
     context "when working on an Edition with a specific Schema" do
-      let(:schema) { build(:embedded_schema, block_type: :travel_advice) }
+      let(:schema) { build(:embedded_schema, block_type: :travel_advice, body: schema_body) }
 
       it "should contain the block_type of the Schema" do
         expect(page).to have_css('*[name^="edition[details][travel_advice]"]', count: 5)
@@ -39,26 +54,40 @@ RSpec.describe Edition::Details::Fields::DateTimeComponent, type: :component do
     end
 
     it "should use the rails multiparameter_attribute format for the field names" do
-      expect(page).to have_css('*[name="edition[details][date_range][start(1i)]"]') # year
-      expect(page).to have_css('*[name="edition[details][date_range][start(2i)]"]') # month
-      expect(page).to have_css('*[name="edition[details][date_range][start(3i)]"]') # day
-      expect(page).to have_css('*[name="edition[details][date_range][start(4i)]"]') # hour
-      expect(page).to have_css('*[name="edition[details][date_range][start(5i)]"]') # minute
+      expect(page).to have_css('*[name="edition[details][date_range][start][date(1i)]"]') # year
+      expect(page).to have_css('*[name="edition[details][date_range][start][date(2i)]"]') # month
+      expect(page).to have_css('*[name="edition[details][date_range][start][date(3i)]"]') # day
+      expect(page).to have_css('*[name="edition[details][date_range][start][time(4i)]"]') # hour
+      expect(page).to have_css('*[name="edition[details][date_range][start][time(5i)]"]') # minute
     end
   end
 
-  describe "when there are existing values" do
-    let(:details) { { "date_range" => { "start" => { "date" => "2001-02-03", "time" => "04:05" } } } }
+  describe "pre-populating values from existing edition details" do
+    context "when the edition has stored date and time values" do
+      let(:details) { { "date_range" => { "start" => { "date" => "2001-02-03", "time" => "04:05" } } } }
 
-    it "should show existing values" do
-      inputs = page.find_all("input")
-      expect(inputs[2].value).to eq("2001")
-      expect(inputs[1].value).to eq("2")
-      expect(inputs[0].value).to eq("3")
+      it "pre-populates the date inputs" do
+        inputs = page.find_all("input")
+        expect(inputs[2].value).to eq("2001") # year
+        expect(inputs[1].value).to eq("2")    # month
+        expect(inputs[0].value).to eq("3")    # day
+      end
 
-      selects = page.find_all("select")
-      expect(selects[0].value).to eq("04")
-      expect(selects[1].value).to eq("05")
+      it "pre-populates the time selects" do
+        selects = page.find_all("select")
+        expect(selects[0].value).to eq("04") # hour
+        expect(selects[1].value).to eq("05") # minute
+      end
+    end
+
+    context "when the edition has no details for this field" do
+      let(:details) { {} }
+
+      it "renders blank inputs without raising an error" do
+        expect(page).to have_css("input", count: 3)
+        expect(page).to have_css("select", count: 2)
+        page.find_all("input").each { |input| expect(input.value).to be_blank }
+      end
     end
   end
 end
