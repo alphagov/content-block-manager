e1a070da9cbfd7f0100e6d1c786d3492907ab269
Graham MacGregor <graham.macgregor@digital.cabinet-office.gov.uk>
2026-03-04 12:45:36 +0000

    TMP: draft

--- engines/block_preview/app/components/block_preview/dynamic_preview_header.rb
+++ engines/block_preview/app/components/block_preview/dynamic_preview_header.rb
@@ -3,4 +3,32 @@ class BlockPreview::DynamicPreviewHeader < ViewComponent::Base
     @block = block
     @title = title
   end
+
+  def options
+    published = { text: render_title(published_edition),
+                  value: "published",
+                  selected: params[:block] != "draft" }
+
+    return [published] if most_recent_edition.id == published_edition.id
+
+    draft = { text: render_title(most_recent_edition),
+              value: "draft",
+              selected: params[:block] == "draft" }
+
+    [draft, published]
+  end
+
+private
+
+  def most_recent_edition
+    @block.document.most_recent_edition
+  end
+
+  def published_edition
+    @block.document.latest_published_edition
+  end
+
+  def render_title(edition)
+    "#{edition.state.humanize} - #{@edition.title}"
+  end
 end
