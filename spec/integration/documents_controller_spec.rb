d884b3fae438354271226e7d89bf15b05371f8fd
Graham MacGregor <graham.macgregor@digital.cabinet-office.gov.uk>
2026-03-05 16:34:08 +0000

    Rescue exceptions from invalid sql statements
    Adding this rescue here means that if our param validation isn't
    robust enough and we end up hitting some DB error, it doesn't show the
    DB error in the UI or any generic error pages. It also prevents the
    hit to Sentry just because a user has entered an invalid param.
    
    Up until now, if a user entered a string that wasn't a valid UUID as
    the lead_organisation, it would show something like this:
    
    ```
    PG::InvalidTextRepresentation: ERROR:  invalid input syntax for type uuid: "dwp" (ActionView::Template::Error)
    CONTEXT:  unnamed portal parameter $10 = '...'
    ```

--- spec/integration/documents_controller_spec.rb
+++ spec/integration/documents_controller_spec.rb
@@ -33,5 +33,44 @@ RSpec.describe DocumentsController, type: :request do
         expect(response.status).to eq(200)
       end
     end
+
+    context "when called with an invalid lead_organisation" do
+      let(:params) { { lead_organisation: "foo" } }
+
+      before do
+        allow_any_instance_of(Document::DocumentFilter)
+          .to receive(:call)
+                .with({})
+                .and_return(Document.none.page)
+
+        allow_any_instance_of(Document::DocumentFilter)
+          .to receive(:call)
+                .with(params)
+                .and_raise(ActiveRecord::StatementInvalid, "bad input")
+      end
+
+      it "should not return a Rails error to the user" do
+        expect { get root_path(nil, params:) }.not_to raise_error
+        expect(response.status).to eq(200)
+      end
+
+      it "should request all documents with no filtering" do
+        expect_any_instance_of(Document::DocumentFilter).to receive(:call).with({})
+
+        get root_path(nil, params:)
+      end
+
+      it "should log the error" do
+        expect(Rails.logger).to receive(:error).with("bad input")
+
+        get root_path(nil, params:)
+      end
+
+      it "should notify Sentry" do
+        expect(GovukError).to receive(:notify).with(ActiveRecord::StatementInvalid)
+
+        get root_path(nil, params:)
+      end
+    end
   end
 end
