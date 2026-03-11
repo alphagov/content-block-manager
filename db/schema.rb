67ddda164eb464044c4d65200013137b04467b54
Ed Davey <ed.davey@digital.cabinet-office.gov.uk>
2026-03-11 17:56:45 +0000

    Create block_editions table with STI support
    Add `block_editions` table using Single Table Inheritance (STI) to support
    different edition types (`TimePeriodEdition`, `TaxEdition`, etc.).
    
    Key design decisions:
    
    - Uses STI via 'type' column (unlike `block_documents` which uses block_type)
    - Common fields (`title`, `description`, `instructions_to_publishers`) on base table
    - No 'details' JSON column - content stored in type-specific tables instead
    - No 'state' column yet - workflow state machine will be added later

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_11_174532) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_11_175427) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -27,6 +27,19 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_11_174532) do
     t.index ["content_id"], name: "index_block_documents_on_content_id", unique: true
     t.index ["content_id_alias"], name: "index_block_documents_on_content_id_alias", unique: true
     t.index ["sluggable_string"], name: "index_block_documents_on_sluggable_string", unique: true
+  end
+
+  create_table "block_editions", force: :cascade do |t|
+    t.bigint "block_document_id", null: false
+    t.datetime "created_at", null: false
+    t.text "description"
+    t.text "instructions_to_publishers"
+    t.integer "lead_organisation_id"
+    t.string "title", null: false
+    t.string "type", null: false
+    t.datetime "updated_at", null: false
+    t.index ["block_document_id"], name: "index_block_editions_on_block_document_id"
+    t.index ["type"], name: "index_block_editions_on_type"
   end
 
   create_table "documents", force: :cascade do |t|
@@ -60,81 +73,82 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_11_174532) do
   end
 
   create_table "edition_authors", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "edition_id", null: false
     t.datetime "created_at", null: false
+    t.integer "edition_id", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
     t.index ["edition_id"], name: "index_edition_authors_on_edition_id"
     t.index ["user_id"], name: "index_edition_authors_on_user_id"
   end
 
   create_table "editions", force: :cascade do |t|
+    t.string "auth_bypass_id"
+    t.text "change_note"
+    t.datetime "created_at", null: false
     t.json "details", null: false
     t.integer "document_id", null: false
-    t.text "state", default: "draft", null: false
-    t.datetime "scheduled_publication"
     t.text "instructions_to_publishers"
-    t.text "title", default: "", null: false
     t.text "internal_change_note"
-    t.text "change_note"
-    t.boolean "major_change"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.uuid "lead_organisation_id"
-    t.string "auth_bypass_id"
+    t.boolean "major_change"
+    t.datetime "scheduled_publication"
+    t.text "state", default: "draft", null: false
+    t.text "title", default: "", null: false
+    t.datetime "updated_at", null: false
     t.datetime "workflow_completed_at", precision: nil
     t.index ["document_id"], name: "index_editions_on_document_id"
     t.index ["title"], name: "index_editions_on_title"
   end
 
   create_table "flipflop_features", force: :cascade do |t|
-    t.text "key", null: false
-    t.boolean "enabled", default: false, null: false
     t.datetime "created_at", null: false
+    t.boolean "enabled", default: false, null: false
+    t.text "key", null: false
     t.datetime "updated_at", null: false
     t.index ["key"], name: "index_flipflop_features_on_key"
   end
 
   create_table "outcomes", force: :cascade do |t|
-    t.bigint "edition_id", null: false
-    t.string "type"
-    t.boolean "skipped"
-    t.string "performer"
-    t.bigint "creator_id", null: false
     t.datetime "created_at", null: false
+    t.bigint "creator_id", null: false
+    t.bigint "edition_id", null: false
+    t.string "performer"
+    t.boolean "skipped"
+    t.string "type"
     t.datetime "updated_at", null: false
     t.index ["creator_id"], name: "index_outcomes_on_creator_id"
     t.index ["edition_id"], name: "index_outcomes_on_edition_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.text "name", null: false
-    t.text "uid"
-    t.text "email"
-    t.boolean "disabled", default: false
-    t.boolean "remotely_signed_out", default: false
-    t.text "permissions"
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "organisation_slug"
+    t.boolean "disabled", default: false
+    t.text "email"
+    t.text "name", null: false
     t.string "organisation_content_id"
+    t.string "organisation_slug"
+    t.text "permissions"
+    t.boolean "remotely_signed_out", default: false
+    t.text "uid"
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email"
     t.index ["uid"], name: "index_users_on_uid"
   end
 
   create_table "versions", force: :cascade do |t|
-    t.text "item_type", null: false
-    t.integer "item_id", null: false
-    t.integer "event", null: false
-    t.text "whodunnit"
-    t.text "state"
-    t.json "field_diffs"
     t.datetime "created_at", null: false
+    t.integer "event", null: false
+    t.json "field_diffs"
+    t.integer "item_id", null: false
+    t.text "item_type", null: false
+    t.text "state"
     t.datetime "updated_at", null: false
+    t.text "whodunnit"
     t.index ["item_id"], name: "index_versions_on_item_id"
     t.index ["item_type"], name: "index_versions_on_item_type"
   end
 
+  add_foreign_key "block_editions", "block_documents"
   add_foreign_key "domain_events", "documents"
   add_foreign_key "domain_events", "editions"
   add_foreign_key "domain_events", "users"
