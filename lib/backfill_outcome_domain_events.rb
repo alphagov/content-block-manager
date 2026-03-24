2a6ddd86601b256738dde8dc766213a64e0accf8
Graham MacGregor <graham.macgregor@digital.cabinet-office.gov.uk>
2026-03-24 14:22:55 +0000

    Remove domain event backfill task
    This task was here to backfill missing domain events for outcomes. Now
    that we no longer associate a domain event with a version when
    creating an outcome, this is no longer needed.

--- lib/backfill_outcome_domain_events.rb
+++
@@ -1,8 +0,0 @@
-class BackfillOutcomeDomainEvents
-  def self.call
-    Outcome.where(domain_event: nil).find_each do |outcome|
-      outcome.send(:create_domain_event)
-      outcome.save!
-    end
-  end
-end
