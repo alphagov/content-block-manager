f224305429fdc74cbb3c89edf25de46d5caecbe1
Graham MacGregor <graham.macgregor@digital.cabinet-office.gov.uk>
2026-03-27 16:11:02 +0000

    remove unused bits from array component

--- app/components/edition/details/fields/array_component.rb
+++ app/components/edition/details/fields/array_component.rb
@@ -7,7 +7,7 @@ private
 
   attr_reader :context
 
-  delegate :field, :name, :id, :error_items, :hint_text, :edition, :schema, :subschema, :object_title, :indexes, to: :context
+  delegate :field, :name, :id, :error_items, :edition, :schema, :subschema, :object_title, :indexes, to: :context
 
   def subschema_block_type
     @subschema_block_type ||= subschema&.block_type
@@ -43,18 +43,6 @@ private
     "array-component-#{edition.id}-#{context.id}"
   end
 
-  def adding_another?
-    params[:add_another] == field.name
-  end
-
-  def button_text
-    if number_of_items.positive? || adding_another?
-      I18n.t("buttons.add_another", item: label.downcase)
-    else
-      I18n.t("buttons.add", item: label.downcase)
-    end
-  end
-
   def component(index)
     Edition::Details::Fields::Array::ItemComponent.new(
       field:,
@@ -62,24 +50,7 @@ private
       schema:,
       value: value[index],
       index: index,
-      can_be_deleted: can_be_deleted?(index),
-      hints: hint_text,
       parent_indexes: indexes,
-    )
-  end
-
-  def can_be_deleted?(index)
-    if index.zero? && field.is_required?
-      false
-    else
-      immutability_checker&.can_be_deleted?(index)
-    end
-  end
-
-  def immutability_checker
-    @immutability_checker ||= EmbeddedObjectImmutabilityCheck.new(
-      edition: edition.document.latest_published_edition,
-      field_reference: [subschema_block_type, object_title, field.name].compact,
     )
   end
 
