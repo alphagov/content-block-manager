d71814083d154d4b429714708e6e4590a01c9cf4
Graham MacGregor <graham.macgregor@digital.cabinet-office.gov.uk>
2026-03-27 16:10:48 +0000

    remove unused bits from item component

--- app/components/edition/details/fields/array/item_component.rb
+++ app/components/edition/details/fields/array/item_component.rb
@@ -1,24 +1,18 @@
 class Edition::Details::Fields::Array::ItemComponent < ViewComponent::Base
   include ErrorsHelper
 
-  def initialize(field:, edition:, schema:, value:, index:, can_be_deleted:, hints:, parent_indexes:)
+  def initialize(field:, edition:, schema:, value:, index:, parent_indexes:)
     @field = field
     @edition = edition
     @schema = schema
     @value = value
     @index = index
-    @can_be_deleted = can_be_deleted
-    @hints = hints || {}
     @parent_indexes = parent_indexes
   end
 
 private
 
-  attr_reader :field, :edition, :schema, :value, :index, :can_be_deleted, :hints, :parent_indexes
-
-  def label
-    "#{field.title.singularize} #{index + 1}"
-  end
+  attr_reader :field, :edition, :schema, :value, :index, :parent_indexes
 
   def components
     fields = field.nested_fields || [field]
@@ -37,18 +31,5 @@ private
       details: value,
       parent_indexes:,
     )
-  end
-
-  def destroy_checkbox
-    field_name = "#{field.name_attribute}[_destroy]"
-    if can_be_deleted
-      render("govuk_publishing_components/components/checkboxes", {
-        name: field_name,
-        items: [{ label: "Delete", value: "1" }],
-        classes: "js-array-item-destroy",
-      })
-    else
-      hidden_field_tag(field_name, 0)
-    end
   end
 end
