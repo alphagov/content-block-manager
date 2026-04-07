fcddbae6f5bf4be9f63150696e2e770b52493ad5
pezholio <pezholio@gmail.com>
2026-04-07 16:51:28 +0100

    Add nokodiff CSS to the block preview asset pipeline
    Configure dartsass to compile nokodiff styles and link the
    resulting CSS in the block_preview engine's asset manifest.

--- config/initializers/dartsass.rb
+++ config/initializers/dartsass.rb
@@ -1,5 +1,6 @@
 Rails.application.config.dartsass.builds = {
   "application.scss" => "application.css",
+  "nokodiff_preview.scss" => "nokodiff.css",
 }
 
 Rails.application.config.dartsass.build_options << " --quiet-deps --silence-deprecation=import --silence-deprecation=mixed-decls"
