401ea4863d38ccd0e7749c9a4eb5386d37f68356
Ed Davey <ed.davey@digital.cabinet-office.gov.uk>
2026-04-27 13:27:46 +0100

    Support 'formats' in config
    A block now has many formats through its schema. These need to align
    with the formats defined in ContentBlockTools.
    
    Perhaps we're moving towards bringing ContentBlockTools into
    ContentBlockManager and exposing its rendering functionality to other
    publishing apps via an API. e.g. so that when the Publishing API publishes
    a document to the Content Store, needing to transform embed codes
    into HTML, rather than:
    
    - rendering content blocks via the content_block_tools gem,
    - it makes an API call to ContentBlockManager

--- app/models/concerns/schema/configuration.rb
+++ app/models/concerns/schema/configuration.rb
@@ -2,9 +2,14 @@ class Schema
   module Configuration
     extend ActiveSupport::Concern
 
+    FORMATS_PROPERTY_KEY = "x-formats".freeze
     BLOCK_DISPLAY_FIELDS_PROPERTY_KEY = "x-block-display-fields".freeze
     EMBEDDABLE_AS_BLOCK_PROPERTY_KEY = "x-embeddable-as-block".freeze
     FIELD_ORDER_PROPERTY_KEY = "x-field-order".freeze
+
+    def formats
+      @body[FORMATS_PROPERTY_KEY] || []
+    end
 
     def block_display_fields
       @body[BLOCK_DISPLAY_FIELDS_PROPERTY_KEY] || []
