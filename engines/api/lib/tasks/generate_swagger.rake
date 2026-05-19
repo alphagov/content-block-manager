eb9bed7a9de2f366b4d6967004060765062cb366
Ed Davey <ed.davey@digital.cabinet-office.gov.uk>
2026-05-19 17:34:50 +0100

    Include realistic response example in OpenAPI spec
    We disable SWAGGER_DRY_RUN in the generate_swagger rake task so that
    rswag executes requests and captures real responses.
    
    And we use a TimePeriod block with realistic organisation names to produce
    a meaningful exampleshowing formats, embed codes, and organisation name.

--- engines/api/lib/tasks/generate_swagger.rake
+++ engines/api/lib/tasks/generate_swagger.rake
@@ -2,6 +2,7 @@ namespace :api do
   desc "Generate Swagger documentation from RSpec tests"
   task generate_swagger: :environment do
     ENV["PATTERN"] = "{spec,engines/*/spec}/**/*_spec.rb"
+    ENV["SWAGGER_DRY_RUN"] = "0"
     Rake::Task["rswag:specs:swaggerize"].invoke
   end
 end
--- engines/api/spec/requests/api_spec.rb
+++ engines/api/spec/requests/api_spec.rb
@@ -3,7 +3,11 @@ require "swagger_helper"
 RSpec.describe "API" do
   path "/blocks/search" do
     let(:organisations) do
-      build_list(:organisation, 3)
+      [
+        build(:organisation, name: "HM Revenue & Customs"),
+        build(:organisation, name: "Foreign, Commonwealth & Development Office"),
+        build(:organisation, name: "Department for Work and Pensions"),
+      ]
     end
 
     before do
@@ -30,7 +34,13 @@ RSpec.describe "API" do
 
       response "200", "blocks found" do
         before do
-          create(:edition, :published, document: create(:document), lead_organisation_id: organisations.first.id)
+          create(
+            :edition,
+            :published,
+            title: "Current Tax Year",
+            document: create(:document, block_type: "time_period", sluggable_string: "current-tax-year"),
+            lead_organisation_id: organisations.first.id,
+          )
         end
 
         schema type: :object,
@@ -70,6 +80,16 @@ RSpec.describe "API" do
                    },
                  },
                }
+
+        after do |example|
+          content = example.metadata[:response][:content] || {}
+          example.metadata[:response][:content] = content.merge(
+            "application/json" => {
+              example: JSON.parse(response.body, symbolize_names: true),
+            },
+          )
+        end
+
         run_test!
       end
 
