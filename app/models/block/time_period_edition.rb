5e670857d36efdcc067af1d73a91af218add78ff
Graham MacGregor <graham.macgregor@digital.cabinet-office.gov.uk>
2026-07-10 09:02:04 +0100

    Associate TimePeriodDateRange with TimePeriodEdition
    Builds the `TimePeriodEdition` model by adding the `date_range` association.
    
    Note:
    
    - `has_one :date_range` association
    - `accepts_nested_attributes_for :date_range` (for form handling)
    
    Co-authored-by: Ed Davey <ed.davey@digital.cabinet-office.gov.uk>

--- app/models/block/time_period_edition.rb
+++ app/models/block/time_period_edition.rb
@@ -1,4 +1,7 @@
 module Block
   class TimePeriodEdition < Edition
+    has_one :date_range, class_name: "Block::TimePeriodDateRange", foreign_key: :edition_id, dependent: :destroy
+
+    accepts_nested_attributes_for :date_range
   end
 end
--- spec/unit/app/models/block/time_period_edition_spec.rb
+++ spec/unit/app/models/block/time_period_edition_spec.rb
@@ -24,4 +24,25 @@ RSpec.describe Block::TimePeriodEdition, type: :model do
       expect(edition.errors[:title]).to include("cannot be blank")
     end
   end
+
+  describe "associations" do
+    it { is_expected.to have_one(:date_range).class_name("Block::TimePeriodDateRange").dependent(:destroy) }
+  end
+
+  describe "nested attributes" do
+    it "accepts nested attributes for date_range" do
+      edition = create(:block_time_period_edition)
+
+      edition.update!(
+        date_range_attributes: {
+          start: Time.zone.parse("2025-04-06 00:00"),
+          end: Time.zone.parse("2026-04-05 23:59"),
+        },
+      )
+
+      expect(edition.date_range).to be_present
+      expect(edition.date_range.start).to eq(Time.zone.parse("2025-04-06 00:00"))
+      expect(edition.date_range.end).to eq(Time.zone.parse("2026-04-05 23:59"))
+    end
+  end
 end
