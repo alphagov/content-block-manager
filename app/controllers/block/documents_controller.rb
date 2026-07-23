e7e5f8f3f12b0481229b257efd8065ef7f2f0b63
Patrick Fleming <Patrick.JosephFleming@dsit.gov.uk>
2026-07-23 15:15:10 +0100

    Add route, controller and basic index page template
    Adds and routes a documents controller with a single index method. For now I’ve just added some static copy to the template, based around what already exists in the ‘v1’ implementation but with the addition of ‘ActiveRecord Blocks’ to clearly signpost the new page.

--- app/controllers/block/documents_controller.rb
+++ app/controllers/block/documents_controller.rb
@@ -1,7 +1,5 @@
 module Block
   class DocumentsController < ApplicationController
-    def index
-      @documents = Block::Document.by_most_recently_created_edition.page(1)
-    end
+    def index; end
   end
 end
