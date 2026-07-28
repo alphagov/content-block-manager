2e273cf6837c0fdc51df8bdb1005b0d769561cb2
Patrick Fleming <Patrick.JosephFleming@dsit.gov.uk>
2026-07-28 14:44:14 +0100

    Render summary list of ActiveRecord documents on index page
    For now this is just a list without filters. That functionality will come in a separate PR.

--- app/controllers/block/documents_controller.rb
+++ app/controllers/block/documents_controller.rb
@@ -1,5 +1,7 @@
 module Block
   class DocumentsController < ApplicationController
-    def index; end
+    def index
+      @documents = Block::Document.by_most_recently_created_edition.page(1)
+    end
   end
 end
