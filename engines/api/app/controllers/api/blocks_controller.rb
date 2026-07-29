c6fa2ee496acf0ca6b585c87c1633c35e6a266bb
Toby Batch <tobias@neontribe.co.uk>
2026-07-29 15:08:06 +0100

    Excludes e2e test objects from the API
    The application excludes e2e test ojects from searches. This ports that
    behavior to the API

--- engines/api/app/controllers/api/blocks_controller.rb
+++ engines/api/app/controllers/api/blocks_controller.rb
@@ -1,23 +1,33 @@
 class Api::BlocksController < Api::ApplicationController
   def search
     result = ContentBlock::Query.call(filters)
-    render json: Api::ResultsPresenter.present(result)
+    filtered_result = filter_testing_artefacts(result)
+    render json: Api::ResultsPresenter.present(filtered_result)
   end
 
   def render_block
     embed_code = params[:embed_code]
-    block = ContentBlock.from_embed_code(Rack::Utils.unescape_path(params[:embed_code].to_s))
-    if block
-      render html: block.render(embed_code)
-    else
-      not_found_page_error "Content block not found for embed code: #{embed_code}"
-    end
+    block = ContentBlock.from_embed_code(Rack::Utils.unescape_path(embed_code.to_s))
+    return not_found_page_error "Content block not found for embed code: #{embed_code}" if block.nil? || testing_artefact_hidden?(block)
+
+    render html: block.render(embed_code)
   end
 
 private
 
   def filters
     params.permit(:block_type, :lead_organisation_id, :keyword)
+  end
+
+  def filter_testing_artefacts(result)
+    return result if Current.user&.is_e2e_user?
+
+    filtered_blocks = result.blocks.reject { |block| block.document.testing_artefact? }
+    ContentBlock::Query::Result.new(blocks: filtered_blocks)
+  end
+
+  def testing_artefact_hidden?(block)
+    block.document.testing_artefact? && !Current.user&.is_e2e_user?
   end
 
   def not_found_page_error(message)
