364e86bc71ebf73164d0a9edc518dbf9e1466c72
Graham MacGregor <graham.macgregor@digital.cabinet-office.gov.uk>
2026-08-07 14:09:18 +0100

    Add feature tests for editing a Time Period Edition
    Checking that you can navigate to the edit page, update the details of
    the edition, view any errors from invalid inputs and successfully save
    the edition.

--- features/step_definitions/v2/edition_steps.rb
+++ features/step_definitions/v2/edition_steps.rb
@@ -2,6 +2,10 @@ When("I am viewing the form to create a new Time Period Edition") do
   visit new_v2_time_period_edition_path
 end
 
+When("I am viewing the form to update an existing Time Period Edition") do
+  visit edit_v2_time_period_edition_path(@time_period_edition)
+end
+
 When("I fill the Time Period Edition details correctly") do
   fill_in_edition_details(
     title: "Test Time Period Edition",
@@ -22,6 +26,23 @@ end
 
 Then("I see that the Edition was created successfully") do
   expect(page).to have_content(I18n.t("block/time_period_edition.create.success"))
+end
+
+Then("I see that the Edition was updated successfully") do
+  expect(page).to have_content(I18n.t("block/time_period_edition.update.success"))
+end
+
+When("a Time Period Edition exists") do
+  @time_period_edition = FactoryBot.create(:v2_time_period_edition)
+  organisation = Organisation.all&.find { |org| org.name == "Ministry of Example" }
+
+  @time_period_edition.update!(
+    title: "New Time Period Edition",
+    description: "This is a new time period edition.",
+    lead_organisation_id: organisation.id,
+    instructions_to_publishers: "Nothing much.",
+  )
+  puts "Created Time Period Edition with ID: #{@time_period_edition.id}"
 end
 
 Then("I see which Time Period Edition errors I need to correct") do
