7972ec19edfa1d111c17f3698792f432f6502dad
Patrick Fleming <Patrick.JosephFleming@dsit.gov.uk>
2026-08-07 12:00:14 +0100

    wip

---
+++ app/components/document/index/v2/filter_options_component.rb
@@ -0,0 +1,52 @@
+require "record_tag_helper/helper"
+
+class Document::Index::V2::FilterOptionsComponent < ViewComponent::Base
+  include ActionView::Helpers::RecordTagHelper
+
+  def initialize(filters:, errors: nil)
+    @filters = filters
+    @errors = errors
+  end
+
+private
+
+  def heading
+    I18n.t("document.index.filter_options.heading")
+  end
+
+  def data_attributes
+    {
+      module: "ga4-search-tracker ga4-form-change-tracker",
+      ga4_search_type: "index-documents",
+      ga4_search_url: helpers.documents_path,
+      ga4_search_section: heading,
+      ga4_search_input_name: "keyword",
+    }
+  end
+
+  def items_for_block_type
+    helpers.valid_schemas.map do |schema|
+      {
+        label: schema.block_type.humanize,
+        value: schema.block_type,
+        checked: @filters.any? && @filters[:block_type]&.include?(schema.block_type),
+      }
+    end
+  end
+
+  def all_organisations_option(selected_orgs)
+    {
+      text: "All organisations",
+      value: "",
+      selected: selected_orgs.compact.empty?,
+    }
+  end
+
+  def taggable_organisations_options(selected_orgs)
+    helpers.taggable_organisations_container(selected_orgs)
+  end
+
+  def options_for_lead_organisation(selected_orgs = [])
+    [all_organisations_option(selected_orgs), taggable_organisations_options(selected_orgs)].flatten
+  end
+end
--- app/controllers/block/documents_controller.rb
+++ app/controllers/block/documents_controller.rb
@@ -1,7 +1,31 @@
 module Block
   class DocumentsController < ApplicationController
     def index
-      @documents = Block::Document.by_most_recently_created_edition.page(1)
+      @filters = params.slice(:keyword, :block_type, :lead_organisation, :page, :last_updated_to, :last_updated_from)
+        .permit!
+        .to_h
+
+      if @filters.any?
+        @filter = Block::Document::DocumentFilter.new(@filters)
+
+        begin
+          @documents = @filter.call
+          render :index
+        rescue Block::Document::DocumentFilter::InvalidFiltersError => e
+          @documents = @filter.call({})
+          @errors = e.errors
+          @error_summary_errors = @errors.map { |error| { text: error.full_message, href: "##{error.attribute}" } }
+          render :index
+        end
+      else
+        redirect_to block_documents_path(default_filters)
+      end
+    end
+
+  private
+
+    def default_filters
+      { lead_organisation: "" }
     end
   end
 end
