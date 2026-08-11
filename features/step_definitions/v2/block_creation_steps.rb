ce59c75889b23e9016180bc4801f25b27d92b4ae
Patrick Fleming <Patrick.JosephFleming@dsit.gov.uk>
2026-08-11 16:37:13 +0100

    Add feature tests for document filtering
    Our testing pyramid now includes:
    1) scope methods tested within the Document model unit tests, that document and confirm what is returned from the database for each scope, and
    2) Document filter unit tests that document and confirm which scopes are called by the filter and when.
    
    This gives us confidence in the nuts and bolts of our filtering mechanism, which means we can afford to have a slimmed down set of feature tests (compared to V1), which confirm that 1) multiple filters can be applied and the results rendered correctly, 2) that errors are handled correctly, 3) that pagination works as expected, and 4) that results are ordered by most recently created edition first.

--- features/step_definitions/v2/block_creation_steps.rb
+++ features/step_definitions/v2/block_creation_steps.rb
@@ -1,23 +1,76 @@
 Given("the following time period content blocks have been drafted:") do |table|
   table.hashes.each do |row|
-    organisation = Organisation.all&.find { |org| org.name == row["Organisation"] }
+    organisation = Organisation.all.find { |org| org.name == row["Organisation"] }
+    updated_at = row["Updated at"].present? ? Date.parse(row["Updated at"]) : Time.zone.now
 
-    document = V2::Document.new(block_type: "time_period")
+    document = V2::Document.create!(
+      block_type: "time_period",
+      updated_at: updated_at,
+    )
 
-    V2::TimePeriodEdition.create!(
+    create(
+      :v2_time_period_edition,
       document: document,
       title: row["Time period name"],
       description: row["Description"],
+      lead_organisation_id: organisation.id,
+      creator: build(:user),
+      created_at: updated_at,
+      updated_at: updated_at,
+    )
+  end
+end
+
+When("I fill in {string} date with {string}, {string}, {string}") do |field_label, day, month, year|
+  param_key = field_label.parameterize(separator: "_")
+
+  fill_in "#{param_key}[3i]", with: day
+  fill_in "#{param_key}[2i]", with: month
+  fill_in "#{param_key}[1i]", with: year
+end
+
+Given("{int} time period content blocks exist") do |count|
+  V2::Document.destroy_all
+
+  organisation = Organisation.all.first
+
+  count.times do |i|
+    document = V2::Document.create!(block_type: "time_period")
+    create(
+      :v2_time_period_edition,
+      document: document,
+      title: "Content Block #{i + 1}",
       lead_organisation_id: organisation.id,
       creator: build(:user),
     )
   end
 end
 
-Then("I should see the details for all three available content blocks") do
-  V2::TimePeriodEdition.find_each do |edition|
-    expect(page).to have_content(edition.title)
-    expect(page).to have_content(edition.description)
-    expect(page).to have_content(edition.lead_organisation.name)
-  end
+Then("I should see {int} content block(s)") do |expected_count|
+  expect(page).to have_css("[data-testid^='homepage-item-']", count: expected_count)
+end
+
+Given(/^a content block exists with title "([^"]*)" created (\d+) (day|days|hour|hours) ago$/) do |title, amount, unit|
+  time_ago = amount.to_i.public_send(unit).ago
+  organisation = Organisation.all.first
+
+  document = V2::Document.create!(block_type: "time_period")
+
+  create(
+    :v2_time_period_edition,
+    document: document,
+    title: title,
+    lead_organisation_id: organisation.id,
+    creator: build(:user),
+    created_at: time_ago,
+  )
+end
+
+Then("I should see {string} listed before {string}") do |first_item, second_item|
+  expect(page).to have_css("[data-testid='homepage-item-0']", text: first_item)
+  expect(page).to have_css("[data-testid='homepage-item-1']", text: second_item)
+end
+
+Then("I should not see {string}") do |content|
+  expect(page).to have_no_content(content)
 end
--- features/v2/view_content_blocks.feature
+++ features/v2/view_content_blocks.feature
@@ -4,10 +4,38 @@ Feature: Search for a content object
     And the organisation "Department of Placeholder" exists
     And the organisation "Ministry of Example" exists
     And the following time period content blocks have been drafted:
-        | Time period name              | Description                             | Organisation              |
-        | British Summer Time           | The period of daylight savings          | Department of Placeholder |
-        | Student loan repayment period | The repayment period for a student loan | Ministry of Example       |
+            | Time period name             | Description                             | Organisation              | Updated at |
+            | British Summer Time          | The period of daylight savings          | Department of Placeholder | 10/05/2026 |
+            | Summer Internship Scheme     | Temporary summer placement scheme       | Ministry of Example       | 10/05/2025 |
+            | Winter Fuel Allowance        | Financial support for winter heating    | Department of Placeholder | 10/05/2026 |
+            | Summer Leave Policy 2024     | Old leave guidance for staff            | Department of Placeholder | 15/06/2024 |
+            | Student loan repayment period| The repayment period for a student loan | Ministry of Example       | 01/02/2026 |
 
-Scenario: view all available content blocks
-    When I visit the V2 index page
-    Then I should see the details for all three available content blocks
+Scenario: GDS Editor can combine filters to find relevant search results
+  When I visit the V2 index page
+  And I enter the keyword "Summer"
+  And I select the lead organisation "Department of Placeholder"
+  And I fill in "Last updated from" date with "01", "01", "2026"
+  When I click to view results
+  Then I should see "British Summer Time"
+  And I should not see "Student loan repayment period"
+
+Scenario: GDS Editor sees errors when searching by invalid dates
+  When I visit the V2 index page
+  And I fill in "Last updated from" date with "41", "01", "2026"
+  And I fill in "Last updated to" date with "13", "13", "2026"
+  When I click to view results
+  Then I should see a message that the filter dates are invalid
+
+Scenario: GDS Editor can view more than one page
+  Given 11 time period content blocks exist
+  When I visit the V2 index page
+  Then I should see 10 content blocks
+  And I click on page 2
+  Then I should see 1 content block
+
+Scenario: Blocks are ordered by most recently created edition
+  Given a content block exists with title "Older document" created 2 days ago
+  And a content block exists with title "Newer document" created 1 hour ago
+  When I visit the V2 index page
+  Then I should see "Newer document" listed before "Older document"
