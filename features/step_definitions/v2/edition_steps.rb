f52f96e5f716594468e88080d11289e8e934162a
Patrick Fleming <Patrick.JosephFleming@dsit.gov.uk>
2026-08-12 11:15:59 +0100

    Rename time period section of en.yml from ‘block’ to ‘v2’
    This was missed in the original renaming.

--- features/step_definitions/v2/edition_steps.rb
+++ features/step_definitions/v2/edition_steps.rb
@@ -25,7 +25,7 @@ When("I fill the Time Period Edition details incorrectly") do
 end
 
 Then("I see that the Edition was created successfully") do
-  expect(page).to have_content(I18n.t("block/time_period_edition.create.success"))
+  expect(page).to have_content(I18n.t("v2/time_period_edition.create.success"))
 end
 
 Then("I see that the Edition was updated successfully") do
